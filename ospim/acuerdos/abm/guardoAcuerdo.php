5f06152a8c318411506f584c78b6025fab4d3e67
tanomartin05 <tanomartin05@gmail.com>
2012-05-09 17:43:45 +0000

    arreglo del for, para que guarde bien las cuotas cuando cargan acuerdos.

--- ospim/acuerdos/abm/guardoAcuerdo.php
+++ ospim/acuerdos/abm/guardoAcuerdo.php
@@ -7,22 +7,22 @@ $usuariomodificacion = $usuarioregistro;
 
 $cuit = $_GET['cuit'];
 $nroacu = $_GET['nroacu'];
-//echo "CUIT: ".$cuit; echo "<br>";
-//echo "ACUERDO: ".$nroacu; echo "<br>"; echo "<br>";
+echo "CUIT: ".$cuit; echo "<br>";
+echo "ACUERDO: ".$nroacu; echo "<br>"; echo "<br>";
 
 $datos = array_values($_POST);
 $sqlAltaCabe = $datos[0];
-//echo "CARGO CABECERA"; echo "<br>";
-//echo $sqlAltaCabe;echo "<br>";echo "<br>";
+echo "CARGO CABECERA"; echo "<br>";
+echo $sqlAltaCabe;echo "<br>";echo "<br>";
 
 //conexion y craecion de transaccion.
 try {
 	$hostname = $_SESSION['host'];
 	$dbname = $_SESSION['dbname'];
-	//echo "$hostname"; echo "<br>";
-	//echo "$dbname"; echo "<br>";
+	echo "$hostname"; echo "<br>";
+	echo "$dbname"; echo "<br>";
 	$dbh = new PDO("mysql:host=$hostname;dbname=$dbname",$_SESSION['usuario'],$_SESSION['clave']);
-	//echo 'Connected to database<br/>';
+	echo 'Connected to database<br/>';
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$dbh->beginTransaction();
 
@@ -31,32 +31,30 @@ try {
 
 	//PERIODOS
 	$cantPer = $datos[1];
-	//echo "CARGO ".$cantPer." PERIODOS <br>";
+	echo "CARGO ".$cantPer." PERIODOS <br>";
 	if ($cantPer != 0) {
 		for ($i = 2; $i<$cantPer+2; $i++) {
 			$sqlAltaPeri = $datos[$i];
-			//echo $sqlAltaPeri;echo "<br>";
+			echo $sqlAltaPeri;echo "<br>";
 			$dbh->exec($sqlAltaPeri);
 		}
 	} else {
-		//echo "NO HAY PERIODOS CARGADOS";echo "<br>";
+		echo "NO HAY PERIODOS CARGADOS";echo "<br>";
 	}
 	
 	$indice = $cantPer + 2;
 	$cantCuotas = $datos[$indice];
 	$iniFor = $indice + 1;
-	$finFor = $indice + $cantCuotas * 6;
-	//echo "<br>";
-	//echo "CARGO ".$cantCuotas." CUOTAS --> INICIO FOR: ".$iniFor." Y TERMINA EN ".$finFor; echo "<br>";
+	$finFor = $iniFor + $cantCuotas * 7;
+	echo "<br>";
+	echo "CARGO ".$cantCuotas." CUOTAS --> INICIO FOR: ".$iniFor." Y TERMINA EN ".$finFor; echo "<br>";
 	$nroCuo = 1;
 	$totalMonto = 0;
-	for ($i= $iniFor; $i <= $finFor; $i++){
+	for ($i= $iniFor; $i < $finFor; $i++){
 		$monto = $datos[$i];
 		$totalMonto = $totalMonto + $monto;
 		$i++;
-		echo $datos[$i];echo "<br>";
 		$fecha = fechaParaGuardar($datos[$i]);
-		echo $fecha;echo "<br>";
 		$i++;
 		$tipoC = $datos[$i];
 		$i++;
@@ -69,11 +67,11 @@ try {
 		$observ = $datos[$i];
 		
 		$sqlCuota="INSERT INTO cuoacuerdosospim VALUES ('$cuit','$nroacu','$nroCuo','$monto','$fecha','$tipoC','$chequen','$chequeb','$chequef','$observ','','','','','','','','$fecharegistro','$usuarioregistro','$fechamodificacion','$usuariomodificacion')";
-		//echo $sqlCuota; echo "<br>";
+		echo $sqlCuota; echo "<br>";
 		$dbh->exec($sqlCuota);	
 		if ($tipoC == 3) {
 			$sqlValCob = "INSERT INTO valoresalcobro VALUES('$cuit','$nroacu','$nroCuo','$chequen','$chequeb','$chequef','','','','','','','')";
-			//echo $sqlValCob; echo "<br>";
+			echo $sqlValCob; echo "<br>";
 			$dbh->exec($sqlValCob);	
 		}
 		$nroCuo++;
