27b44055842580e5e3cdf5a41bd8b17fdec5938a
tanomartin05 <tanomartin05@gmail.com>
2012-05-30 14:48:49 +0000

    cancerlar cuota pegando a la base

--- ospim/acuerdos/cancelacion/cancelarCuota.php
+++ ospim/acuerdos/cancelacion/cancelarCuota.php
@@ -17,23 +17,23 @@ $resCab = mysql_query($sqlCab,$db);
 $rowCab = mysql_fetch_array($resCab);
 
 $montoAcuerdo = $rowCab['montoacuerdo'];
-echo "MONTO ACUERDO: ".$montoAcuerdo; echo "<br>";
+ //echo "MONTO ACUERDO: ".$montoAcuerdo;  //echo "<br>";
 
-echo "<br>";
+ //echo "<br>";
 
 $montoAPagar = $rowCab['montoapagar'];
-echo "MONTO A PAGAR: ".$montoAPagar; echo "<br>";
+ //echo "MONTO A PAGAR: ".$montoAPagar;  //echo "<br>";
 $cuotasAcuerdo = $rowCab['cuotasapagar'];
-echo "CUOTAS ACUERDO: ".$cuotasAcuerdo; echo "<br>";
+ //echo "CUOTAS ACUERDO: ".$cuotasAcuerdo;  //echo "<br>";
 
-echo "<br>";
+ //echo "<br>";
 
 $montoPagado = $rowCab['montopagadas'];
-echo "MONTO PAGADO: ".$montoPagado; echo "<br>";
+ //echo "MONTO PAGADO: ".$montoPagado;  //echo "<br>";
 $cuotasPagadas = $rowCab['cuotaspagadas'];
-echo "CUOTAS PAGADAS: ".$cuotasPagadas; echo "<br>";
+ //echo "CUOTAS PAGADAS: ".$cuotasPagadas;  //echo "<br>";
 
-echo "<br>";
+ //echo "<br>";
 
 $sqlCuo = "select * from cuoacuerdosospim where cuit = $cuit and nroacuerdo = $acuerdo and nrocuota = $cuota";
 $resCuo = mysql_query($sqlCuo,$db); 
@@ -41,19 +41,19 @@ $rowCuo = mysql_fetch_array($resCuo);
 
 $montoCuota = $rowCuo['montocuota'];
 $fechaPagada = fechaParaGuardar($fechapagada);
-echo "MONTO CUOTA A CANCELAR: ".$montoCuota; echo "<br>";
-echo "FECHA DE PAGO: ".$fechaPagada; echo "<br>";
-echo "OBSERVACIONES: ".$observ; echo "<br>";
+ //echo "MONTO CUOTA A CANCELAR: ".$montoCuota;  //echo "<br>";
+ //echo "FECHA DE PAGO: ".$fechaPagada;  //echo "<br>";
+ //echo "OBSERVACIONES: ".$observ;  //echo "<br>";
 
-echo "<br>";
+ //echo "<br>";
 
 $montoPagadoUpdate = $montoPagado + $montoCuota;
 $cuotasPagasUpdate = $cuotasPagadas + 1;
 
-echo "MONTO PAGADO A UPDETEAR: ".$montoPagadoUpdate; echo "<br>";
-echo "CUOTAS PAGADAS A UPDETEAR: ".$cuotasPagasUpdate; echo "<br>";
+ //echo "MONTO PAGADO A UPDETEAR: ".$montoPagadoUpdate;  //echo "<br>";
+ //echo "CUOTAS PAGADAS A UPDETEAR: ".$cuotasPagasUpdate;  //echo "<br>";
 
-echo "<br>";
+ //echo "<br>";
 
 $estadoAcuerdo = 1;
 if ($cuotasPagasUpdate == $cuotasAcuerdo && $montoPagadoUpdate == $montoAPagar) {
@@ -62,7 +62,7 @@ if ($cuotasPagasUpdate == $cuotasAcuerdo && $montoPagadoUpdate == $montoAPagar) 
 
 $saldoAcuerdo = $montoAPagar -  $montoPagadoUpdate;
 
-echo "SALDO ACUERDO A UPDETEAR: ".$saldoAcuerdo; echo "<br>"; echo "<br>";
+ //echo "SALDO ACUERDO A UPDETEAR: ".$saldoAcuerdo;  //echo "<br>";  //echo "<br>";
 
 try {
 	$hostname = $_SESSION['host'];
@@ -72,18 +72,18 @@ try {
 	$dbh->beginTransaction();
 	
 	$sqlUpdateCuota = "UPDATE cuoacuerdosospim set montopagada = $montoCuota, observaciones = $observ, fechapagada = $fechaPagada, fechacancelacion = $fechamodificacion, sistemacancelacion = 'M', fechamodificacion = $fechamodificacion, usuariomodificacion = $usuariomodificacion where cuit = $cuit and nroacuerdo = $acuerdo and nrocuota = $cuota";
-	echo $sqlUpdateCuota;echo "<br>";
-	//$dbh->exec($sqlUpdateCuota); 
+	 //echo $sqlUpdateCuota; //echo "<br>";
+	$dbh->exec($sqlUpdateCuota); 
 	$sqlUpdateCabe = "UPDATE cabacuerdosospim set montopagadas = $montoPagadoUpdate, cuotaspagadas = $cuotasPagasUpdate, estadoacuerdo = $estadoAcuerdo, fechapagadas = $fechamodificacion, saldoacuerdo = $saldoAcuerdo where cuit = $cuit and nroacuerdo = $acuerdo";
-	echo $sqlUpdateCabe; echo "<br>";
-	//$dbh->exec($sqlUpdateCabe); 
+	 //echo $sqlUpdateCabe;  //echo "<br>";
+	$dbh->exec($sqlUpdateCabe); 
 	
-	//$dbh->commit();
-	//$pagina = "selecCanCuotas.php?cuit=$cuit&acuerdo=$acuerdo";
-	//Header("Location: $pagina"); 
+	$dbh->commit();
+	$pagina = "selecCanCuotas.php?cuit=$cuit&acuerdo=$acuerdo";
+	Header("Location: $pagina"); 
 	
 }catch (PDOException $e) {
-	echo $e->getMessage();
+	 echo $e->getMessage();
 	$dbh->rollback();
 }
 
