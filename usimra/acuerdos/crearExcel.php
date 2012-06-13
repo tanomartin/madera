ad562509ac97d4482b72abb15144300eb4901e12
tanomartin05 <tanomartin05@gmail.com>
2012-06-13 14:50:08 +0000

    prueba excel...

--- usimra/acuerdos/crearExcel.php
+++ usimra/acuerdos/crearExcel.php
@@ -1,87 +1,48 @@
-<? session_save_path("sessiones");
-session_start();
-if($_SESSION['usuario'] == null or $_SESSION['aut'] > 1)
-	header ("location:index.htm");
-?>
-<?php include("../controlSession.php");?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Generador Listado</title>
-</head>
-
 <?php
-include("conexion.php");
-$sql = "select * from usuarios where id = '$_SESSION[usuario]'";
-$result = mysql_db_query("acuerdos",$sql,$db);
-$row=mysql_fetch_array($result);
+/** Error reporting */
+error_reporting(E_ALL);
+
+/** Include path **/
+ini_set('include_path', ini_get('include_path').';../Classes/');
+
+/** PHPExcel */
+include 'PHPExcel.php';
+
+/** PHPExcel_Writer_Excel2007 */
+include 'PHPExcel/Writer/Excel2007.php';
+
+// Create new PHPExcel object
+echo date('H:i:s') . " Create new PHPExcel object\n";
+$objPHPExcel = new PHPExcel();
+
+// Set properties
+echo date('H:i:s') . " Set properties\n";
+$objPHPExcel->getProperties()->setCreator("Maarten Balliauw");
+$objPHPExcel->getProperties()->setLastModifiedBy("Maarten Balliauw");
+$objPHPExcel->getProperties()->setTitle("Office 2007 XLSX Test Document");
+$objPHPExcel->getProperties()->setSubject("Office 2007 XLSX Test Document");
+$objPHPExcel->getProperties()->setDescription("Test document for Office 2007 XLSX, generated using PHP classes.");
+
+
+// Add some data
+echo date('H:i:s') . " Add some data\n";
+$objPHPExcel->setActiveSheetIndex(0);
+$objPHPExcel->getActiveSheet()->SetCellValue('A1', 'Hello');
+$objPHPExcel->getActiveSheet()->SetCellValue('B2', 'world!');
+$objPHPExcel->getActiveSheet()->SetCellValue('C1', 'Hello');
+$objPHPExcel->getActiveSheet()->SetCellValue('D2', 'world!');
+
+// Rename sheet
+echo date('H:i:s') . " Rename sheet\n";
+$objPHPExcel->getActiveSheet()->setTitle('Simple');
+
+		
+// Save Excel 2007 file
+echo date('H:i:s') . " Write to Excel2007 format\n";
+$objWriter = new PHPExcel_Writer_Excel2007($objPHPExcel);
+$objWriter->save(str_replace('.php', '.xlsx', __FILE__));
+
+// Echo done
+echo date('H:i:s') . " Done writing file.\r\n";
+
 ?>
-<body bgcolor="#E4C192" link="#D5913A" vlink="#CF8B34" alink="#D18C35">
-<p align="center"><img border="0" src="top.jpg" width="700" height="120"></p>
-<div align="center">
-  <p>
-<?php
-include("excelwriter.inc.php");  
-$nombre = "InformeCheques/listaCheques".date('hisjmy').".xls";
-
-$excel=new ExcelWriter($nombre);
-    
-if($excel==false) {   
-	echo $excel->error;
-}
-//Escribimos la primera fecha de hoy
-$today = date("j/n/Y");
-$myArr=array($today);
-$excel->writeLine($myArr);
-$excel->writeRow();
-
-//Escribimos la primera fila con las cabeceras
-$myArr=array("Cod. Del.","Cod. Emp.","Razon Social","Nro. Cheque","Banco","Nro. Acuerdo","Nro. Cuota","Importe");
-$excel->writeLine($myArr);
-
-$cheques = $_POST['cheques'];
-$lista = explode("\n",$cheques);
-$cantidad = sizeof($lista);
-$resultados = 0;
-$boletas = 0;
-
-for ( $i = 0; $i <= $cantidad - 1 ; $i++) {
-	$sql = "select depositos.delcod, depositos.empcod, empresas.nombre, depositos.nrocheque, depositos.banco, depositos.importe, depositos.nroacu, depositos.nrocuo
-	from 
-	depositos, empresas 
-	where 
-	depositos.nrocheque = $lista[$i] and
-	depositos.nrcuit = empresas.nrcuit and
-	depositos.delcod = empresas.delcod and
-	depositos.empcod = empresas.empcod";
-	$result = mysql_db_query("acuerdos",$sql,$db);
-	$cant = mysql_num_rows($result);
-	if ($cant > 0) {;
-		$resultados = $resultados + 1;
-		while ($row = mysql_fetch_array($result)) {
-			$boletas = $boletas + 1;
-			$myArr=array($row['delcod'],$row['empcod'],$row['nombre'],$row['nrocheque'],$row['banco'],$row['nroacu'],$row['nrocuo'],$row['importe']);
-			$excel->writeLine($myArr);
-		}
-	} else {
-		echo $lista[$i]." NO EXISTE <br>";
-	}
-}
-$excel->writeRow();
-$total="TOTAL";
-$myArr=array($total, $boletas);
-$excel->writeLine($myArr);
-$excel->close();
-if ($resultados == 0) {
-	$resultados = "ninguno";
-}
-echo "<br>";
-echo "<b>Se buscaron $cantidad y se encontraron $resultados</b>";
-?> 
-</p>
-  <p>&nbsp;    </p>
-  <div align="center"><a href="cargarCheques.php"><font color="#CD8C34" face="Verdana" size="2"><b>Volver</b></font></a></div>
-</body>
-</html>
