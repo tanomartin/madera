af52617b9a9e2a8e22037d19ce11cedb20cbfb92
tanomartin05 <tanomartin05@gmail.com>
2012-09-20 18:01:12 +0000

    cambio en el menu y sus links

--- ospim/empresas/abm/empresa.php
+++ ospim/empresas/abm/empresa.php
@@ -59,26 +59,48 @@ jQuery(function($){
 	?>
 </p>
   <form name="empresa" id="empresa" method="post" action="guardarEmpresa.php">
-    <table width="743" height="98" border="0">
+    <table width="836" height="87" border="0">
       <tr>
-        <td width="57">C.U.I.T. </td>
-        <td width="253"><label>
+        <td width="57" height="41">C.U.I.T. </td>
+        <td width="157"><label>
           <input name="cuit" type="text" id="cuit" value="<?php echo $row['cuit'];?>">
         </label></td>
-        <td width="117"><label>Razón Social</label></td>
-        <td width="298"><input name="nombre" type="text" id="nombre" value="<?php echo $row['nombre'];?>"></td>
+        <td width="213"><label>Razón Social</label></td>
+        <td width="144"><input name="nombre" type="text" id="nombre" value="<?php echo $row['nombre'];?>"></td>
+        <td width="79">Domicilio</td>
+        <td width="160"><input name="domicilio" type="text" id="domicilio" value="<?php echo $row['domilegal'];?>"></td>
       </tr>
       <tr>
-        <td><label>Domicilio</label></td>
-        <td><input name="domicilio" type="text" id="domicilio" value="<?php echo $row['domilegal'];?>"></td>
-        <td><label>Localidad</label></td>
-        <td><input name="localidad" type="text" id="localidad" value="<?php echo $rowlocalidad['nomlocali'];?>"></td>
-      </tr>
-      <tr>
-        <td><label>Provincia</label></td>
-        <td><input name="provincia" type="text" id="provincia" value="<?php echo $rowprovi['descrip']; ?>"></td>
+        <td height="40"><label>Provincia</label></td>
+        <td><select name="tipoAcuerdo" size="1" id="tipoAcuerdo">
+              <option value=0>Seleccione un valor </option>
+              <?php 
+					$sqlProvi="select * from provincia";
+					$resProvi= mysql_query($sqlProvi,$db);
+					while ($rowProvi=mysql_fetch_array($resProvi)) { 	
+						if ($rowProvi['codprovin'] == $row['codprovin']) {?>			
+              <option value="<?php echo $rowProvi['codprovin'] ?>" selected="selected"><?php echo $rowProvi['descrip']  ?></option>
+                  <?php } else { ?>
+              <option value="<?php echo $rowProvi['codprovin'] ?>"><?php echo $rowProvi['descrip']  ?></option>
+                  <?php } ?>          
+                <?php } ?>
+            </select></td>
         <td><label>Codigo Postal</label></td>
         <td><input name="codPos" type="text" id="codPos" value="<?php echo $row['numpostal'];?>"></td>
+        <td>Localidad</td>
+        <td><select name="select" size="1" id="select">
+          <option value=0>Seleccione un valor </option>
+          <?php 
+					$sqlLaca="select * from localidades";
+					$resLoca= mysql_query($sqlLaca,$db);
+					while ($rowLoca=mysql_fetch_array($resLoca)) { 	
+						if ($rowLoca['codlocali'] == $row['codlocali']) {?>
+          <option value="<?php echo $rowLoca['codlocali'] ?>" selected="selected"><?php echo $rowLoca['nomlocali']  ?></option>
+          <?php } else { ?>
+          <option value="<?php echo $rowLoca['codlocali'] ?>"><?php echo $rowLoca['nomlocali']  ?></option>
+          <?php } ?>
+          <?php } ?>
+        </select></td>
       </tr>
     </table>
     <p>
