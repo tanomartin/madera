c1d1035e5045f43370647f8674a42008d808b4d9
tanomartin05 <tanomartin05@gmail.com>
2012-10-10 12:47:58 +0000

    cambio de comprobacion si viene del menu prinical o porque paso tiempo y murio la session.

--- usimra/logout.php
+++ usimra/logout.php
@@ -1,7 +1,10 @@
 <?php session_save_path("sessiones");
 session_start();
 session_destroy();
-if ($_SERVER['HTTP_REFERER'] == "http://localhost/usimra/menu.php") {
+$cadena = $_SERVER['HTTP_REFERER'];
+$buscar = "menu.php";
+$resultado = strpos($cadena, $buscar);
+if ($resultado == true) {
 	header ('location:index.php');
 } else {
 	header ('location:sesionCaducada.php');
