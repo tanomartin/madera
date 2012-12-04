5f2e859a7510781eda4cc437c3d7a9cb4b1b2cf7
tanomartin05 <tanomartin05@gmail.com>
2012-12-04 12:49:40 +0000

    Agragado buscador de boletas.

--- usimra/acuerdos/impresion/menuBoletas.php
+++ usimra/acuerdos/impresion/menuBoletas.php
@@ -20,15 +20,17 @@ A:hover {text-decoration: none;color:#00FFFF }
 <div align="center">
   <p><font color="#000000" size="2" face="Verdana, Arial, Helvetica, sans-serif"><strong><a href="../menuAcuerdos.php">VOLVER</a></strong></font></p>
   <p><span class="Estilo2">Men&uacute; Boletas </span></p>
-  <table width="412" border="3">
+  <table width="614" border="3">
     <tr>
       <td width="196"><p align="center">Anulacion de Boletas </p>
 	  	  <!-- href="cargaAnulacion.php" -->
 		  <!-- href="../../moduloNoDisponible.php" -->
           <p align="center"><a href="cargaAnulacion.php" ><img src="../img/anulacion.jpg" width="98" height="90" border="0" alt="enviar"/></a></p>
         <p align="center">&nbsp;</p></td>
-      <td width="196"><p align="center">Impresi&oacute;n de Boletas </p>
-          <p align="center"><a class="enlace" href="moduloImpresion.php"><img src="../img/impresora.jpg" width="98" height="84" border="0" alt="enviar"/></a></p>
+      <td width="196" valign="top"><p align="center">Impresi&oacute;n de Boletas </p>
+      <p align="center"><a class="enlace" href="moduloImpresion.php"><img src="../img/impresora.jpg" width="98" height="84" border="0" alt="enviar"/></a></p></td>
+      <td width="196"><p align="center">Buscador de Boletas </p>
+        <p align="center"><a class="enlace" href="buscadorBoleta.php"><img src="../img/lupa.jpg" width="98" height="84" border="0" /> </a></p>
         <p align="center">&nbsp;</p></td>
     </tr>
   </table>
