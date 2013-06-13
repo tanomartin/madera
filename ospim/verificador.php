8bf0c78b16bb084328b7c6e0024c7491763e718f
tanomartin05 <tanomartin05@gmail.com>
2013-06-13 15:39:06 +0000

    Cambio en el ingreso en ospim

--- ospim/verificador.php
+++ ospim/verificador.php
@@ -2,25 +2,24 @@
 session_start();
 
 $datos = array_values($_POST);
-$usuario = $datos [0];
-$clave = $datos [1];
+$usuario = $datos[0];
+$clave = $datos[1];
 $host = "localhost";
-
+if ($_SESSION['usuario'] == $usuario) {
+	header ('location:index.php?error=2');	
+}
 $dbusuario =  mysql_connect($host,$usuario, $clave);
 if (!$dbusuario) {
-   echo("<p><strong><a href='index.htm'><font face='Verdana' size='2'><b>VOLVER AL LOGIN</b></font></a></strong></p>");
-   die('No pudo conectarse: ' . mysql_error());
+  	header ('location:index.php?error=1');	
+} else {
+	$_SESSION['host']= $host;
+	$_SESSION['usuario'] = $usuario;
+	$_SESSION['clave'] = $clave;
+	$_SESSION['aut'] = 1;
+	$_SESSION['dbname'] = "madera";
+	$_SESSION['ultimoAcceso'] = date("Y-n-j H:i:s");
+	header ('location:menu.php');
 }
-
-$_SESSION['host']= $host;
-$_SESSION['usuario'] = $usuario;
-$_SESSION['clave'] = $clave;
-$_SESSION['aut'] = 1;
-$_SESSION['dbname'] = "madera";
-
-$_SESSION['ultimoAcceso'] = date("Y-n-j H:i:s");
-
-header ('location:menu.php');
 ?>
 
 
