0772346aadfe0925e0c0272bc22c715726b37e32
tanomartin05 <tanomartin05@gmail.com>
2013-07-12 14:16:21 +0000

    Agregado en del "0000-00-00" cuando no hay fecha en al funcion para fecha para guardar.

--- lib/fechas.php
+++ lib/fechas.php
@@ -23,6 +23,9 @@ function getAnio($fecha) {
 }
 
 function fechaParaGuardar($fecha) {
+	if ($fecha == "") {
+		return("0000-00-00");
+	}
 	$dia = substr($fecha,0,2);
 	$mes = substr($fecha,3,2);
 	$anio = substr($fecha,6,4);
