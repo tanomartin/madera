e412dc493fc4e3452955517775af7db2e23e9b3b
tanomartin05 <tanomartin05@gmail.com>
2013-09-02 16:54:55 +0000

    Sacamos las columnas que muestra la cant de acuerdos activos y no activos en gestores e inspectores.

--- ospim/fiscalizacion/configuracion/gestoresinspectores/inspectores.php
+++ ospim/fiscalizacion/configuracion/gestoresinspectores/inspectores.php
@@ -22,54 +22,20 @@ A:hover {text-decoration: none;color:#00FFFF }
     <input type="reset" name="volver" value="Volver" onclick="location.href = '../menuConfiguracionFiscalizacion.php'" align="center"/>
 </p>
   <p><span class="Estilo2">Inspectores</span></p>
-  <table border="1" width="732" bordercolorlight="#000099" bordercolordark="#0066FF" bordercolor="#000000" cellpadding="2" cellspacing="0">
+  <table border="1" width="500" bordercolorlight="#000099" bordercolordark="#0066FF" bordercolor="#000000" cellpadding="2" cellspacing="0">
     <tr>
-      <td width="49"><div align="center"><strong><font size="1" face="Verdana">Codigo</font></strong></div></td>
-      <td width="320"><div align="center"><strong><font size="1" face="Verdana">Apellido y Nombre</font></strong></div></td>
-	  <td width="126"></td>
-	  <td width="93"><div align="center"><strong><font size="1" face="Verdana">Acuerdos</font></strong></div></td>
-	  <td width="112"><div align="center"><strong><font size="1" face="Verdana">Acuerdos Activos</font></strong></div></td>
+      <td width="50"><div align="center"><strong><font size="1" face="Verdana">Codigo</font></strong></div></td>
+      <td width="400"><div align="center"><strong><font size="1" face="Verdana">Apellido y Nombre</font></strong></div></td>
+	  <td width="50"></td>
     </tr>
     <?php	
 		$sqlInspectores = "select * from inspectores group by codigo order by apeynombre";
 		$resInspectores = mysql_query($sqlInspectores,$db); 
 		while ($rowInspectores = mysql_fetch_array($resInspectores)) { 
 				print("<tr>");
-				print ("<td width=49><div align=center><font face=Verdana size=1>".$rowInspectores['codigo']."</font></div></td>");
-				print ("<td width=320><div align=center><font face=Verdana size=1>".$rowInspectores['apeynombre']."</font></div></td>");
-				print ("<td width=126><div align=center><font face=Verdana size=1><a href='modificarInspector.php?codigo=".$rowInspectores['codigo']."'>Modificar</a></font></div></td>");
-				
-				$sqlAcuerdosOspim = "select * from cabacuerdosospim where inspectorinterviene = $rowInspectores[codigo]";
-				$resAcuerdosOspim = mysql_query($sqlAcuerdosOspim,$db); 
-				$ospimCant = mysql_num_rows($resAcuerdosOspim); 
-				
-				$sqlAcuerdosUsimra = "select * from cabacuerdosusimra where inspectorinterviene = $rowInspectores[codigo]";
-				$resAcuerdosUsimra = mysql_query($sqlAcuerdosUsimra,$db); 
-				$usimraCant = mysql_num_rows($resAcuerdosUsimra); 
-				
-				$controlAcu = $ospimCant + $usimraCant;
-				
-				if ($controlAcu == 0) {
-					print ("<td width=93><div align=center><font face=Verdana size=1>Eliminar</font></div></td>");
-				} else {
-					print ("<td width=93><div align=center><font face=Verdana size=1>".$controlAcu."</font></div></td>");
-				}
-				
-				$sqlActivoOspim = "select * from cabacuerdosospim where inspectorinterviene = $rowInspectores[codigo] and estadoacuerdo = 0";
-				$resActivoOspim = mysql_query($sqlActivoOspim,$db); 
-				$ospimCantActivo = mysql_num_rows($resActivoOspim); 
-				
-				$sqlActivoUsimra = "select * from cabacuerdosusimra where inspectorinterviene = $rowInspectores[codigo] and estadoacuerdo = 0";
-				$resActivoUsimra = mysql_query($sqlActivoUsimra,$db); 
-				$usimraCantActivo = mysql_num_rows($resActivoUsimra); 
-				
-				$controlAcuActivos = $ospimCantActivo + $usimraCantActivo;
-				
-				if ($controlAcuActivos == 0) {
-					print ("<td width=112><div align=center><font face=Verdana size=1>Desactivar</font></div></td>");
-				} else {
-					print ("<td width=112><div align=center><font face=Verdana size=1>".$controlAcuActivos."</font></div></td>");
-				}
+				print ("<td width=50><div align=center><font face=Verdana size=1>".$rowInspectores['codigo']."</font></div></td>");
+				print ("<td width=400><div align=center><font face=Verdana size=1>".$rowInspectores['apeynombre']."</font></div></td>");
+				print ("<td width=50><div align=center><font face=Verdana size=1><a href='modificarInspector.php?codigo=".$rowInspectores['codigo']."'>Modificar</a></font></div></td>");
 				print ("</tr>"); 
 			}
 	  ?>
