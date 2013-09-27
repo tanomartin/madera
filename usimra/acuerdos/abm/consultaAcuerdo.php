8c97e25f33912107fd929dbd8328a36f562e0ea0
tanomartin05 <tanomartin05@gmail.com>
2013-09-27 13:29:12 +0000

    agregAgregado de consultas de acuerdos para empresas de baja para usimra.

--- usimra/acuerdos/abm/consultaAcuerdo.php
+++ usimra/acuerdos/abm/consultaAcuerdo.php
@@ -22,23 +22,7 @@ A:hover {text-decoration: none;color:#00FFFF }
   </div>
   <div align="center">
     <?php 
-	//PARA LA CABECERA
-	$sql = "select * from empresas where cuit = $cuit";
-	$result = mysql_query($sql,$db); 
-	$row = mysql_fetch_array($result); 
-	
-	$sqlDelEmp = "select * from delegaempresa where cuit = $cuit";
-	$resDelEmp = mysql_query($sqlDelEmp,$db);
-	$rowDelEmp = mysql_fetch_array($resDelEmp); 
-	
-	$sqllocalidad = "select * from localidades where codlocali = $row[codlocali]";
-	$resultlocalidad = mysql_query($sqllocalidad,$db); 
-	$rowlocalidad = mysql_fetch_array($resultlocalidad); 
-	
-	$sqlprovi =  "select * from provincia where codprovin = $row[codprovin]";
-	$resultprovi = mysql_query($sqlprovi,$db); 
-	$rowprovi = mysql_fetch_array($resultprovi);
-	//FIN CABECERA
+	include($libPath."cabeceraEmpresaConsulta.php"); 
 	include($libPath."cabeceraEmpresa.php"); 
 	
 	$sqlCabecera = "select * from cabacuerdosusimra where cuit = $cuit and nroacuerdo = $nroacu";
