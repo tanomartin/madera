8ec022580a836aef95a485825fd2dd49b985c07d
tanomartin05 <tanomartin05@gmail.com>
2013-10-02 19:20:02 +0000

    subida de link para ver cta cte de ospim

--- comun/empresas/abm/empresa.php
+++ comun/empresas/abm/empresa.php
@@ -104,7 +104,7 @@ function rediSabanaCtaCte(origen) {
       </div></td>
       <td width="123"><div align="center">
         <?php if ($origen == "ospim") { ?>
-			<input name="ctacteOspim" type="button" value="Cuenta Corriente">
+			<input name="ctacteOspim" type="button" value="Cuenta Corriente" onClick="rediSabanaCtaCte('ospim')">
 		<?php } else {?>
 			<input name="ctacteUsimra" type="button" value="Cuenta Corriente">
 		<?php } ?>
