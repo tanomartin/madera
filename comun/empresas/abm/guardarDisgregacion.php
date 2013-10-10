6a3b6efb3db61e10c3b8eb99a9b6f2524099d0d4
tanomartin05 <tanomartin05@gmail.com>
2013-10-10 15:18:52 +0000

    Agregado de envio de mail para cuando se modifica una disgregacion dineraria

--- comun/empresas/abm/guardarDisgregacion.php
+++ comun/empresas/abm/guardarDisgregacion.php
@@ -21,24 +21,23 @@ for ($i=0; $i<$cantJuris*2; $i++) {
 		$dbh = new PDO("mysql:host=$hostname;dbname=$dbname",$_SESSION['usuario'],$_SESSION['clave']);
 		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$dbh->beginTransaction();
-		$dbh->exec($sqlUpdateDisgregacion);
-		
-		
-		$username = "sistemas@ospim.com.ar";
-		$passw = "pepepascual";
-		$fromRepli = "Sistemas O.S.P.I.M.";
-		$subject = "Se ha efectuado una disgregación dineraria";
-		$bodymail = "<body><br><br>Este es un mensaje de Aviso.<br><br>En el CUIT: <strong>".$cuit."</strong>, se ha efectuado un cambio en la disgregación dineraria.";
-		$address = "jlgomez@usimra.com.ar";
-		envioMail($username, $passw, $fromRepli, $subject, $bodymail, $address);
-		
+		$dbh->exec($sqlUpdateDisgregacion);	
 		$dbh->commit();
 		$pagina = "empresa.php?cuit=$cuit&origen=$origen";
 		Header("Location: $pagina"); 
 	}catch (PDOException $e) {
 		echo $e->getMessage();
 		$dbh->rollback();
-	}
-	
+	}	
 }
+
+//ENVIO DE MAIL AVISO A GOMEZ
+$username = "sistemas@ospim.com.ar";
+$passw = "pepepascual";
+$fromRepli = "Sistemas O.S.P.I.M.";
+$subject = "Se ha efectuado una disgregación dineraria";
+$bodymail = "<body><br><br>Este es un mensaje de Aviso.<br><br>En el CUIT: <strong>".$cuit."</strong>, se ha efectuado un cambio en la disgregación dineraria.";
+$address = "jlgomez@usimra.com.ar";
+envioMail($username, $passw, $fromRepli, $subject, $bodymail, $address);
+
 ?>
--- comun/empresas/abm/guardarDisgregacionNueva.php
+++ comun/empresas/abm/guardarDisgregacionNueva.php
@@ -25,15 +25,6 @@ try {
 		//print($sqlUpdateDisgregacion);print("<br>");
 		$dbh->exec($sqlUpdateDisgregacion);
 	}
-
-	$username = "sistemas@ospim.com.ar";
-	$passw = "pepepascual";
-	$fromRepli = "Sistemas O.S.P.I.M.";
-	$subject = "Se ha efectuado una disgregación dineraria";
-	$bodymail = "<body><br><br>Este es un mensaje de Aviso.<br><br>En el CUIT: <strong>".$cuit."</strong>, se ha efectuado un cambio en la disgregación dineraria por el agregado de una jurisdicción.";
-	$address = "jlgomez@usimra.com.ar";
-	envioMail($username, $passw, $fromRepli, $subject, $bodymail, $address);
-
 	$dbh->commit();
 	$pagina = "empresa.php?cuit=$cuit&origen=$origen";
 	Header("Location: $pagina"); 
@@ -43,4 +34,12 @@ try {
 	$dbh->rollback();
 }
 
+//ENVIO DE MAIL AVISO A GOMEZ
+$username = "sistemas@ospim.com.ar";
+$passw = "pepepascual";
+$fromRepli = "Sistemas O.S.P.I.M.";
+$subject = "Se ha efectuado una disgregación dineraria";
+$bodymail = "<body><br><br>Este es un mensaje de Aviso.<br><br>En el CUIT: <strong>".$cuit."</strong>, se ha efectuado un cambio en la disgregación dineraria por el agregado de una jurisdicción.";	
+$address = "jlgomez@usimra.com.ar";
+envioMail($username, $passw, $fromRepli, $subject, $bodymail, $address);
 ?>
