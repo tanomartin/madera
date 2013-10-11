1d3f522ce24614e20a82dfe3f950def7a7fea3ea
tanomartin05 <tanomartin05@gmail.com>
2013-10-11 20:25:56 +0000

    Filtro los pagos con los coneptos 381 y 401 solo estos.

--- comun/empresas/abm/cuentas/cuentaCorrienteOspim.php
+++ comun/empresas/abm/cuentas/cuentaCorrienteOspim.php
@@ -107,7 +107,7 @@ function reverificaFueraTermino($ano, $me, $db) {
 
 function encuentroPagos($db) {
 	global $cuit, $anoinicio, $mesinicio, $anofin, $mesfin;
-	$sqlPagos = "select anopago, mespago, fechapago from afipprocesadas where cuit = $cuit and concepto != 'REM' and ((anopago > $anoinicio and anopago <= $anofin) or (anopago = $anoinicio and mespago >= $mesinicio)) group by anopago, mespago, fechapago";
+	$sqlPagos = "select anopago, mespago, fechapago from afipprocesadas where cuit = $cuit and (concepto = '381' or concepto = '401') and ((anopago > $anoinicio and anopago <= $anofin) or (anopago = $anoinicio and mespago >= $mesinicio)) group by anopago, mespago, fechapago";
 	$resPagos = mysql_query($sqlPagos,$db);
 	$CantPagos = mysql_num_rows($resPagos); 
 	if($CantPagos > 0) {
