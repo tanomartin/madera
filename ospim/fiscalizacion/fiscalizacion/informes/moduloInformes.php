18d4e7b9462e25c675abd3bb5e003c3700f05079
tanomartin05 <tanomartin05@gmail.com>
2013-11-20 15:55:48 +0000

    agregado de menu informes

---
+++ ospim/fiscalizacion/fiscalizacion/informes/moduloInformes.php
@@ -0,0 +1,52 @@
+<?php include($_SERVER['DOCUMENT_ROOT']."/lib/controlSessionOspim.php"); ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>.: Módulo Fiscalisador OSPIM :.</title>
+<style type="text/css">
+<!--
+.Estilo1 {
+	font-size: 18px;
+	font-weight: bold;
+}
+-->
+</style>
+</head>
+<style>
+A:link {text-decoration: none;color:#0033FF}
+A:visited {text-decoration: none}
+A:hover {text-decoration: none;color:#00FFFF }
+</style>
+
+<body bgcolor="#CCCCCC">
+	<div align="center">
+	  <p>
+	    <input type="reset" name="volver" value="Volver" onClick="location.href = '../menuFiscalizaciones.php'" align="center"/>
+      </p>
+	  <table width="626" border="3">
+        <tr>
+          <td width="200"><p align="center">Aportes</p>
+             <a class="enlace" href="#"><p align="center"><img src="../img/consultas.png" width="90" height="90" border="0" alt="enviar"/></p>
+            <p align="center">&nbsp;</p></td>
+          <td width="200"><p align="center">DDJJ</p>
+              <p align="center"><a class="enlace" href="#"><img src="../img/consultas.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+            <p align="center">&nbsp;</p></td>
+          <td width="200"><p align="center">Requerimientos </p>
+              <p align="center"><a class="enlace" href="~#"><img src="../img/consultas.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+            <p align="center">&nbsp;</p></td>
+        </tr>
+        <tr>
+          <td><p align="center">&nbsp;</p>
+              <p align="center">&nbsp;</p></td>
+          <td><p align="center">Liquidaciones</p>
+              <p align="center"><a class="enlace" href="#"><img src="../img/consultas.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+            <p>&nbsp;</p></td>
+          <td>&nbsp;</td>
+        </tr>
+      </table>
+	  <p>&nbsp;</p>
+</div>
+</body>
+</html>
