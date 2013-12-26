33ad862e5f90c2c8f328291449482c0182497ab6
tanomartin05 <tanomartin05@gmail.com>
2013-12-26 15:54:11 +0000

    index para el aplicativo usimra ospim

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+<style>
+
+A:link {text-decoration: none}
+A:visited {text-decoration: none}
+A:hover {text-decoration:underline; color:FCF63C}
+
+.Estilo1 {
+	font-size: 24px;
+	font-weight: bold;
+}
+.Estilo2 {color: 0}
+</style>
+
+<title>.: O.S.P.I.M. :.</title>
+</head>
+
+<body link="#D5913A" vlink="#CF8B34" alink="#D18C35">
+<form method="POST" action="verificador.php">
+  <div align="center">
+    <p class="Estilo1">Ingreso Sistema</p>
+    <table width="512" border="0">
+      <tr>
+        <td width="250"><div align="center" class="Estilo2">
+          <p><a href="ospim/index.php"><img src="ospim/img/logo.png" width="220" height="200" border="0" /></a></p>
+          <p><strong>O.S.P.I.M.</strong></p>
+        </div></td>
+        <td width="246"><div align="center" class="Estilo2">
+          <p><a href="usimra/index.php"><img src="usimra/img/logo.jpg" width="200" height="200" border="0" /></a></p>
+          <p><strong>U.S.I.M.R.A.</strong></p>
+        </div></td>
+      </tr>
+    </table>
+    <p>&nbsp;</p>
+  </div>
+</form>
+
+</body>
+
+</html>
