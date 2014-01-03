fc1d2f14605d39ab2e3abbc0bbf2a6057365bd21
tanomartin05 <tanomartin05@gmail.com>
2014-01-03 16:20:16 +0000

    subida menu legales

--- ospim/menu.php
+++ ospim/menu.php
@@ -39,6 +39,11 @@ function abrirFiscalizacion(dire) {
 	"toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=800, height=500, top=30, left=40");
 }
 
+function abrirLegales(dire) {
+	c= window.open(dire,"Legales",
+	"toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=800, height=500, top=30, left=40");
+}
+
 function abrirSistemas(dire) {
 	c= window.open(dire,"Sistemas",
 	"toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=800, height=500, top=30, left=40");
@@ -51,45 +56,69 @@ function abrirSistemas(dire) {
 <div align="center">
   <p><span class="Estilo1">Men&uacute; Principal - Sistema OSPIM</span></p>
   <p><img src="img/logo.png" width="168" height="142" /></p>
-  <table width="599" border="2">
+  <table width="600" border="1">
     <tr>
-      <td width="196"> <div align="center">
+      <td> 
+	  <div align="center">
         <p>Acuerdos</p>
         <p><a href="javascript:abrirAcuerdos('acuerdos/menuAcuerdos.php')"><img src="img/acuerdos.png" width="90" height="90" border="0"/></a></p>
         <p>&nbsp;</p>
-        </div></td>
-      <td width="196"><div align="center">
+        </div>
+	  </td>
+      <td>
+	  <div align="center">
 	    <p>Empresas</p>
 	    <p><a href="javascript:abrirEmpresa('../comun/empresas/menuEmpresa.php?origen=ospim')"><img src="img/empresa.png" width="90" height="90" border="0" alt="enviar"/></a></p>
 	    <p>&nbsp;</p>
-      </div></td>
+      </div>
+	  </td>
 	  
-      <td width="183"><div align="center">
+      <td>
+	  <div align="center">
 	    <p>Afiliados</p>
 	    <p><a href="javascript:abrirAfil('afiliados/menuAfiliados.php')"><img src="img/afiliados.png" width="90" height="90" border="0" /></a></p>
 	    <p>&nbsp;</p>
-      </div></td>
+      </div>
+	  </td>
     </tr>
+	
 	<tr>
-      <td width="196"><div align="center">
+      <td>
+	  <div align="center">
 	    <p>Fiscalizaci&oacute;n</p>
 	    <p><a href="javascript:abrirFiscalizacion('fiscalizacion/menuFiscalizacion.php')"><img src="img/fiscalizacion.png" width="90" height="90" border="0" alt="enviar"/></a></p>
 	    <p>&nbsp;</p>
-      </div></td>
+      </div>
+	  </td>
 		
       <td width="196"><div align="center">
 	    <p>Auditoria Medica </p>
 	    <p><a href="javascript:abrirAuditoria('auditoria/menuAuditoria.php')"><img src="img/auditoria.png" width="90" height="90" border="0" alt="enviar"/></a></p>
 	    <p>&nbsp;</p>
       </div></td> 
+		
+	  <td>
+	  <div align="center">
+	    <p>Legales </p>
+	    <p><a href="javascript:abrirLegales('legales/menuLegales.php')"><img src="img/juicios.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+	    <p>&nbsp;</p>
+      </div>
+	  </td>
+	  
+    </tr>
 	  <?php if ($_SESSION['usuario'] == 'sistemas') { ?>
-		  <td width="183"><div align="center">
+	<tr>
+	  <td>&nbsp;</td>
+	  <td>
+	  <div align="center">
 			<p>Sistemas</p>
 			<p><a href="javascript:abrirSistemas('sistemas/menuSistemas.php')"><img src="img/sistemas.png" width="90" height="90" border="0" alt="enviar"/></a></p>
 			<p>&nbsp;</p>
-		  </div></td>
-	 <?php } ?>
+	  </div>
+	  </td>
+	  <td>&nbsp;</td>
     </tr>
+	 <?php } ?>
   </table>
    <p><strong><a href="logout.php">
      <input type="button" name="salir" value="SALIR" onclick="location.href='logout.php'" />
