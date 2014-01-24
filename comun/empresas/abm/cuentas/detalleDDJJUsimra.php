9274a8a39569674689d8a9160aeaaa96c8218ccf
tanomartin05 <tanomartin05@gmail.com>
2014-01-24 19:54:38 +0000

    agregado de detalles de usimra pagos y ddjj

---
+++ comun/empresas/abm/cuentas/detalleDDJJUsimra.php
@@ -0,0 +1,87 @@
+<?php include($_SERVER['DOCUMENT_ROOT']."/lib/controlSession.php");
+include($_SERVER['DOCUMENT_ROOT']."/lib/fechas.php");
+
+$cuit=$_GET['cuit'];
+$sql = "select * from empresas where cuit = $cuit";
+$result = mysql_query($sql,$db); 
+$row = mysql_fetch_array($result); 
+
+$anio=$_GET['anio'];
+$mes=$_GET['mes'];
+
+$sqlDdjj = "select *
+			from tempddjjusimra 
+			where 
+			cuit = $cuit and 
+			anoddjj = $anio and 
+			mesddjj = $mes and
+			cuil = '99999999999' order by nrocontrol ASC";
+
+$resDdjj = mysql_query($sqlDdjj,$db);
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<style>
+A:link {text-decoration: none;color:#0033FF}
+A:visited {text-decoration: none}
+A:hover {text-decoration: none;color:#00FFFF }
+</style>
+
+<script language="javascript">
+function abrirInfo(dire) {
+	a= window.open(dire,"DetalleDDJJUsimra",
+	"toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=800, height=500, top=10, left=10");
+}
+</script>
+</head>
+
+<title>.: DDJJ Empresa :.</title>
+</head>
+<body bgcolor=<?php echo $bgcolor ?>>
+<div align="center">
+  <table width="774" border="1">
+    <tr>
+      <td width="242">C.U.I.T.: <b><?php echo $cuit ?></b></td>
+      <td width="516">Nombre: <b><?php echo $row['nombre'] ?></b></td>
+    </tr>
+	 <tr>
+      <td colspan="2">Peridodo: <b><?php echo $mes."-".$anio ?></b></td>
+	</tr>
+  </table>
+  <p><strong>Infomación DDJJ</strong></p>
+  
+   <table border="1">
+    <tr>
+      <th>Generación</th>
+      <th>Personal</th>
+	  <th>Remuneracion</th>
+	  <th>Aporte 0.6%</th>
+	  <th>Contri 1%</th>
+	  <th>Aporte 1.5%</th>
+	  <th>Recargo</th>
+	  <th>Declarado</th>
+	  <th>Observ</th>
+	  <th>Acciones</th>
+    </tr>
+
+<?php while ($rowDdjj = mysql_fetch_assoc($resDdjj)) {	 ?>	
+	 <tr>
+	    <td><?php print(substr($rowDdjj['nrocontrol'],6,2)."-".substr($rowDdjj['nrocontrol'],4,2)."-".substr($rowDdjj['nrocontrol'],0,4)." ".substr($rowDdjj['nrocontrol'],8,2).":".substr($rowDdjj['nrocontrol'],10,2).":".substr($rowDdjj['nrocontrol'],12,2))?></td>
+	  	<td align="center"><?php print($rowDdjj['cantidadpersonal']) ?></td>
+		<td align="right"><?php print(number_format($rowDdjj['remuneraciones'],2,',','.')) ?></td>
+		<td align="right"><?php print(number_format($rowDdjj['apor060'],2,',','.')) ?></td>
+		<td align="right"><?php print(number_format($rowDdjj['apor100'],2,',','.')) ?></td>
+		<td align="right"><?php print(number_format($rowDdjj['apor150'],2,',','.')) ?></td>
+		<td align="right"><?php print(number_format($rowDdjj['recargo'],2,',','.')) ?></td>
+		<td align="right"><?php print(number_format($rowDdjj['totalaporte'],2,',','.')) ?></td>
+		<td align="center"><?php print($rowDdjj['observacion']) ?></td>
+		<td align="center"><a href=javascript:abrirInfo('verDetalleDDJJUsimra.php?nrocontrol=<?php echo $rowDdjj['nrocontrol'] ?>&cuit=<?php echo $rowDdjj['cuit'] ?>&mes=<?php echo $rowDdjj['mesddjj'] ?>&anio=<?php echo $rowDdjj['anoddjj'] ?>')>Detalle</a></td>
+	 </tr>
+<?php } ?>
+  </table>
+  
+</div>
+</body>
+
---
+++ comun/empresas/abm/cuentas/detallePagosUsimra.php
@@ -0,0 +1,112 @@
+<?php include($_SERVER['DOCUMENT_ROOT']."/lib/controlSession.php");
+include($_SERVER['DOCUMENT_ROOT']."/lib/fechas.php");
+
+$cuit=$_GET['cuit'];
+$sql = "select * from empresas where cuit = $cuit";
+$result = mysql_query($sql,$db); 
+$row = mysql_fetch_array($result); 
+
+$anio=$_GET['anio'];
+$mes=$_GET['mes'];
+
+$sqlPagos = "select s.*, ap6.importe as importeap6, ap1.importe as importeap1, ap15.importe as importeap15
+from seguvidausimra s
+LEFT OUTER JOIN
+apor060usimra ap6 on
+s.cuit = ap6.cuit and
+s.anopago = ap6.anopago and
+s.mespago = ap6.mespago and
+s.nropago = ap6.nropago
+LEFT OUTER JOIN
+apor100usimra ap1 on
+s.cuit = ap1.cuit and
+s.anopago = ap1.anopago and
+s.mespago = ap1.mespago and
+s.nropago = ap1.nropago
+LEFT OUTER JOIN
+apor150usimra ap15 on
+s.cuit = ap15.cuit and
+s.anopago = ap15.anopago and
+s.mespago = ap15.mespago and
+s.nropago = ap15.nropago
+where 
+s.cuit = $cuit and 
+s.anopago = $anio and 
+s.mespago = $mes";
+
+$resPagos = mysql_query($sqlPagos,$db); 
+$i = 0;
+$totalPagado = 0;
+while ($rowPagos = mysql_fetch_assoc($resPagos)) {
+	$pagos[$i] = $rowPagos;
+	$totalPagado = (float) ($totalPagado + $pagos[$i]['montopagado']);
+	$i = $i + 1;
+}
+
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<style>
+A:link {text-decoration: none;color:#0033FF}
+A:visited {text-decoration: none}
+A:hover {text-decoration: none;color:#00FFFF }
+</style>
+</head>
+
+<title>.: Pagos Empresa :.</title>
+</head>
+<body bgcolor=<?php echo $bgcolor ?>>
+<div align="center">
+  <table width="774" border="1">
+    <tr>
+      <td width="242">C.U.I.T.: <b><?php echo $cuit ?></b></td>
+      <td width="516">Nombre: <b><?php echo $row['nombre'] ?></b></td>
+    </tr>
+	 <tr>
+      <td colspan="2">Peridodo: <b><?php echo $mes."-".$anio ?></b></td>
+	</tr>
+  </table>
+  <p><strong>Detalles del Pago</strong></p>
+  <table border="1">
+    <tr>
+      <th>Nro. Pago</th>
+      <th>Fecha de Pago </th>
+	  <th>Personal</th>
+	  <th>Remuneraci&oacute;n</th>
+	  <th>Aporte 0.6%</th>
+	  <th>Contri 1%</th>
+	  <th>Aporte 1.5%</th>
+	  <th>Recargo</th>
+	  <th>Pagado</th>
+	  <th>Codigo Barra</th>
+	  <th>Observ</th>
+    </tr>
+	<?php
+	for ($n=0; $n < sizeof($pagos); $n++) { 
+		$nroPago = $n+1; 
+		print("<tr align='center'>");
+		print("<td>".$nroPago."</td>");
+		print("<td>".invertirFecha($pagos[$n]['fechapago'])."</td>");
+		print("<td>".$pagos[$n]['cantidadpersonal']."</td>");
+		print("<td align='right'>".number_format($pagos[$n]['remuneraciones'],2,',','.')."</td>");
+		print("<td align='right'>".number_format($pagos[$n]['importeap6'],2,',','.')."</td>");
+		print("<td align='right'>".number_format($pagos[$n]['importeap1'],2,',','.')."</td>");
+		print("<td align='right'>".number_format($pagos[$n]['importeap15'],2,',','.')."</td>");
+		print("<td align='right'>".number_format($pagos[$n]['montorecargo'],2,',','.')."</td>");
+		print("<td align='right'>".number_format($pagos[$n]['montopagado'],2,',','.')."</td>");
+		print("<td>".$pagos[$n]['codigobarra']."</td>");
+		print("<td>".$pagos[$n]['observaciones']."</td>");
+		print("</tr>");
+	}?>
+	<tr>
+      <td colspan="8"><div align="right"><strong>TOTAL</strong></div></td>
+<?php print("<td><div align='right'><b>".number_format($totalPagado,2,',','.')."</b></div></td>");?>
+	<td colspan="2"></td>
+    </tr>
+  </table>
+  
+</div>
+</body>
+
