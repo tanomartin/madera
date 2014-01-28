30be6b7049caa92f8f8c0dbf0c03c9940824cc83
tanomartin05 <tanomartin05@gmail.com>
2014-01-28 18:51:20 +0000

    Agregado de modificacion de inspeccion TODO: liquidar y consultar inspeccion antes de liquidar.

---
+++ usimra/fiscalizacion/fiscalizacion/requerimientos/guardarDatosInpseccion.php
@@ -0,0 +1,40 @@
+<?php $libPath = $_SERVER['DOCUMENT_ROOT']."/lib/";
+include($libPath."controlSessionUsimra.php"); 
+include($libPath."fechas.php"); 
+
+$fechamodif = date("Y-m-d H:m:s");
+$usuariomodif = $_SESSION['usuario'];
+
+$datos = array_values($_POST);
+$nroreq = $datos[0];
+$inspector = $datos[1];
+$fechaasig = fechaParaGuardar($datos[2]);
+$dias = $datos[3];
+$addoc = $datos[4];
+$detalledoc = $datos[5];
+$formaenvio = $datos[6];
+$fecharecibo = fechaParaGuardar($datos[7]);
+$fechadevolu = fechaParaGuardar($datos[8]);
+$inspefec = $datos[9];
+$fechainsp = fechaParaGuardar($datos[10]);
+//var_dump($datos);
+
+$sqlUpdateInspec = "UPDATE inspecfiscalizusimra set inspectorasignado = $inspector, fechaasignado = '$fechaasig', diasefectivizacion = $dias, adjuntadocumentos = $addoc, detalledocumentos = '$detalledoc', formaenviodocumentos = $formaenvio, fecharecibodocumentos = '$fecharecibo',  fechadevoluciondocumentos = '$fechadevolu', inspeccionefectuada = $inspefec, fechainspeccion = '$fechainsp', fechamodificacion = '$fechamodif', usuariomodificacion = '$usuariomodif' WHERE nrorequerimiento = $nroreq";
+//print($sqlUpdateInspec);
+
+try {
+	$hostname = $_SESSION['host'];
+	$dbname = $_SESSION['dbname'];
+	$dbh = new PDO("mysql:host=$hostname;dbname=$dbname",$_SESSION['usuario'],$_SESSION['clave']);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$dbh->beginTransaction();
+	$dbh->exec($sqlUpdateInspec);
+	$dbh->commit();
+	$pagina = "listarInspecciones.php";
+	Header("Location: $pagina"); 
+}catch (PDOException $e) {
+	echo $e->getMessage();
+	$dbh->rollback();
+}
+
+?>
---
+++ usimra/fiscalizacion/fiscalizacion/requerimientos/listarInspecciones.php
@@ -0,0 +1,120 @@
+<?php $libPath = $_SERVER['DOCUMENT_ROOT']."/lib/";
+include($libPath."controlSessionUsimra.php"); 
+
+$sqlReque = "SELECT * from reqfiscalizusimra where procesoasignado = 2 and requerimientoanulado = 0 order by nrorequerimiento DESC";
+$resReque = mysql_query($sqlReque,$db);
+$canReque = mysql_num_rows($resReque);
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>.: Listado de Requerimientos en Inpsección:.</title>
+</head>
+<style>
+A:link {text-decoration: none;color:#0033FF}
+A:visited {text-decoration: none}
+A:hover {text-decoration: none;color:#00FFFF }
+.Estilo2 {
+	font-weight: bold;
+	font-size: 18px;
+}
+</style>
+
+<script src="/lib/funcionControl.js" type="text/javascript"></script>
+<script src="/lib/jquery.js"></script>
+<script src="/lib/jquery-ui.min.js"></script>
+<link rel="stylesheet" href="/lib/jquery.tablesorter/themes/theme.blue.css">
+<script src="/lib/jquery.tablesorter/jquery.tablesorter.js"></script>
+<script src="/lib/jquery.tablesorter/jquery.tablesorter.widgets.js"></script>
+<script src="/lib/jquery.tablesorter/addons/pager/jquery.tablesorter.pager.js"></script> 
+<script type="text/javascript">
+
+$(function() {
+		$("#listado")
+		.tablesorter({
+			theme: 'blue', 
+			widthFixed: true, 
+			widgets: ["zebra", "filter"], 
+			headers:{5:{sorter:false}},
+			widgetOptions : { 
+				filter_cssFilter   : '',
+				filter_childRows   : false,
+				filter_hideFilters : false,
+				filter_ignoreCase  : true,
+				filter_searchDelay : 300,
+				filter_startsWith  : false,
+				filter_hideFilters : false,
+			}
+		})
+		.tablesorterPager({container: $("#paginador")}); 
+	});
+	
+</script>
+
+<body bgcolor="#B2A274">
+<div align="center">
+  <p><span style="text-align:center">
+    <input type="reset" name="volver" value="Volver" onclick="location.href = '../menuFiscalizaciones.php'" align="center"/>
+  </span></p>
+  	<p class="Estilo2">Listado de  Requerimiento en Inspecci&oacute;n  </p>
+	<table class="tablesorter" id="listado" style="width:1000px; font-size:14px">
+		<thead>
+			  <tr>
+				<th>Número</th>
+				<th>Origen</th>
+				<th>Solicitante</th>
+				<th>Motivo</th>
+				<th>Cuit</th>
+				<th>Datos Inpección</th>
+			  </tr>
+		</thead>
+		<tbody>
+		  <?php while($rowReque = mysql_fetch_array($resReque)) { 
+		  			if ($rowReque['origenrequerimiento'] == 1) {
+						$origen = "Fiscalización";
+					}
+					if ($rowReque['origenrequerimiento'] == 2) {
+						$origen = "Afiliaciones";
+					}
+					if ($rowReque['origenrequerimiento'] == 3) {
+						$origen = "Prestación";
+					} 
+			?>
+				  <tr align="center">
+						<td><?php echo $rowReque['nrorequerimiento'] ?></td>
+						<td><?php echo $origen ?></td>   
+						<td><?php echo $rowReque['solicitarequerimiento'] ?></td>   
+						<td><?php echo $rowReque['motivorequerimiento'] ?></td>   
+						<td><?php echo $rowReque['cuit'] ?></td>   
+						<td><a href='datosInspeccion.php?nroreq=<?php echo $rowReque['nrorequerimiento'] ?>'>Modificar</a></td>   
+				  </tr>
+		<?php }?>
+     	</tbody>
+	</table>
+	<table width="245" border="0">
+      <tr>
+        <td width="239">
+		<div id="paginador" class="pager">
+		  <form>
+			<p align="center">
+			  <img src="../img/first.png" width="16" height="16" class="first"/> <img src="../img/prev.png" width="16" height="16" class="prev"/>
+			  <input name="text" type="text" class="pagedisplay" style="background:#CCCCCC; text-align:center" size="8" readonly="readonly"/>
+		    <img src="../img/next.png" width="16" height="16" class="next"/> <img src="../img/last.png" width="16" height="16" class="last"/>
+		    <select name="select" class="pagesize">
+		      <option selected="selected" value="10">10 por pagina</option>
+		      <option value="20">20 por pagina</option>
+		      <option value="30">30 por pagina</option>
+		      <option value="<?php echo $canReque;?>">Todos</option>
+		      </select>
+		    </p>
+		  </form>	
+		</div>
+	</td>
+      </tr>
+  </table>
+</div>
+</body>
+</html>
