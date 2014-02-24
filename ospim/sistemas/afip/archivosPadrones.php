ddaa97905c9a5a1d75cb53fe871bc46eb3d545d8
jcbolognese <jcbolognese@gmail.com>
2014-02-24 21:42:24 +0000

    Finalizacion del Tratamiento AFIP para archivos de Padrones por DDJJ

--- ospim/sistemas/afip/archivosPadrones.php
+++ ospim/sistemas/afip/archivosPadrones.php
@@ -92,7 +92,7 @@ if($noHayPadron) {
 								$vacios = substr($registros[$i], 120, 4);
 								$codobs = substr($registros[$i], 124, 6);
 
-								$registrosalida = str_replace(' ', '', $proximonro).'|'.str_replace(' ', '', $nroregistro).'||'.str_replace(' ', '', $codobs).'|'.str_replace(' ', '', $anoddjj).'|'.str_replace(' ', '', $mesddjj).'|'.str_replace(' ', '', $cuit).'|'.str_replace(' ', '', $cuil).'|'.str_replace(' ', '', $remundeclara).'|'.str_replace(' ', '', $impadicional).'|'.str_replace(' ', '', $familiares).'|'.str_replace(' ', '', $adherentes).'|'.str_replace(' ', '', $secuenciapresentacion).'|'.str_replace(' ', '', $apoadicional).'|'.str_replace(' ', '', $remdecreto).'|'.str_replace(' ', '', $uniesposa);
+								$registrosalida = str_replace(' ', '', $proximonro).'|'.str_replace(' ', '', $nroregistro).'|'.str_replace(' ', '', $cuit).'|'.$nombre.'|'.$calle.'|'.(int)$numero.'|'.str_replace(' ', '', $piso).'|'.str_replace(' ', '', $depto).'|'.$localidad.'|'.(int)$provincia.'|'.(int)$codpos.'|'.str_replace(' ', '', $vacios).'|'.str_replace(' ', '', $codobs);
 								fwrite($punteroarchivo, $registrosalida."\n");
 								$registrosleidos = $registrosleidos + 1;
 							} else {
@@ -110,147 +110,42 @@ if($noHayPadron) {
 										$dbl->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 										$dbl->beginTransaction();
 
-										$sqlAddDisco = "INSERT INTO nominasddjj (nrodisco, fechaarchivoafip, fechaemailafip, registrosafip, fechaprocesoospim, usuarioprocesoospim, registrosprocesoospim, carpetaarchivoospim) VALUES (:nrodisco,:fechaarchivoafip,:fechaemailafip,:registrosafip,:fechaprocesoospim,:usuarioprocesoospim,:registrosprocesoospim,:carpetaarchivoospim)";
+										$sqlAddDisco = "INSERT INTO padronesddjj (nrodisco, fechaarchivoafip, fechaemailafip, registrosafip, fechaprocesoospim, usuarioprocesoospim, registrosprocesoospim, carpetaarchivoospim) VALUES (:nrodisco,:fechaarchivoafip,:fechaemailafip,:registrosafip,:fechaprocesoospim,:usuarioprocesoospim,:registrosprocesoospim,:carpetaarchivoospim)";
 										$resAddDisco = $dbl->prepare($sqlAddDisco);
 										if($resAddDisco->execute(array(':nrodisco' => $proximonro, ':fechaarchivoafip' => $fechatransfcorta, ':fechaemailafip' => $fechamensaje, ':registrosafip' => (int)$totalregistros, ':fechaprocesoospim' => $fechahoy, ':usuarioprocesoospim' => $usuarioproceso, ':registrosprocesoospim' => (int)$registrosleidos, ':carpetaarchivoospim' => $archivo_salida))) {
 											
-											$sqlLoadArchivo = "LOAD DATA LOCAL INFILE '$archivo_salida' REPLACE INTO TABLE afipddjj FIELDS TERMINATED BY '|' LINES TERMINATED BY '\n'";
+											$sqlLoadArchivo = "LOAD DATA LOCAL INFILE '$archivo_salida' REPLACE INTO TABLE afippadrones FIELDS TERMINATED BY '|' LINES TERMINATED BY '\n'";
 											$linkid = mysqli_init();
 											mysqli_options($linkid, MYSQLI_OPT_LOCAL_INFILE, true);
 											mysqli_real_connect($linkid, $hostname, $_SESSION['usuario'], $_SESSION['clave'], $dbname);
 											$resLoadArchivo = mysqli_query($linkid, $sqlLoadArchivo);
 											if (!$resLoadArchivo) {
 												$tituloform = "ERROR";
-												$mensaje = 'La carga de los registros de DDJJ (AFIPDDJJ) desde el archivo FALLO.';
+												$mensaje = 'La carga de los registros de Padron por DDJJ (AFIPPADRONES) desde el archivo FALLO.';
 											} else {
-												$sqlCabeceraDJ = "SELECT cuit, anoddjj, mesddjj, COUNT(cuil) AS totalpersonal, SUM(remundeclarada) AS totalremundeclarada, IF(anoddjj>=2007,0.00,(SUM(remundecreto))) AS totalremundecreto FROM afipddjj WHERE nrodisco = '$proximonro' AND anoddjj >= 2000 GROUP BY cuit, anoddjj, mesddjj, secuenciapresentacion ORDER BY cuit, anoddjj, mesddjj, secuenciapresentacion";
-												$resCabeceraDJ = $dbl->query($sqlCabeceraDJ);
-												if (!$resCabeceraDJ){
-													$tituloform = "ERROR";
-													$mensaje = 'El proceso de agrupamiento de los registros para la cabecera de DDJJ FALLO.';
-												} else {
-													$archivo_agrupa = $destino_ddjj."NOCA".$proximonro.".txt";
-													$punteroagrupado = fopen($archivo_agrupa, 'w') or die("Hubo un error al generar el archivo de agrupamientos para la cabecera de DDJJ");
-													foreach ($resCabeceraDJ as $agrupado){
-														$registroagrupado = $agrupado[cuit].'|'.$agrupado[anoddjj].'|'.$agrupado[mesddjj].'|'.$agrupado[totalpersonal].'|'.$agrupado[totalremundeclarada].'|'.$agrupado[totalremundecreto];
-														fwrite($punteroagrupado, $registroagrupado."\n");
-													}
-													fclose($punteroagrupado);
-													
-													$sqlLoadCabeceraDJ = "LOAD DATA LOCAL INFILE '$archivo_agrupa' REPLACE INTO TABLE cabddjjospim FIELDS TERMINATED BY '|' LINES TERMINATED BY '\n'";
-													$resLoadCabeceraDJ = mysqli_query($linkid, $sqlLoadCabeceraDJ);
-													if (!$resLoadCabeceraDJ) {
-														$tituloform = "ERROR";
-														$mensaje = 'La carga de los registros de agrupamiento para la cabecera de DDJJ (CABDDJJOSPIM) FALLO.';
-													} else {
-														$sqlDetalleDJ = "SELECT cuit, anoddjj, mesddjj, cuil, IF(anoddjj>=2007,remundeclarada,(remundeclarada+remundecreto)) AS remundeclarada, adherentes FROM afipddjj WHERE nrodisco = '$proximonro' AND anoddjj >= 2000 ORDER BY cuit, anoddjj, mesddjj, cuil, secuenciapresentacion";
-														$resDetalleDJ = $dbl->query($sqlDetalleDJ);
-														if (!$resDetalleDJ){
-															$tituloform = "ERROR";
-															$mensaje = 'El proceso de seleccion de los registros para el detalle de DDJJ FALLO.';
-														} else {
-															$archivo_detalle = $destino_ddjj."NODE".$proximonro.".txt";
-															$punterodetalle = fopen($archivo_detalle, 'w') or die("Hubo un error al generar el archivo de seleccion para el detalle de DDJJ");
-															foreach ($resDetalleDJ as $detalle){
-																$registrodetalle = $detalle[cuit].'|'.$detalle[anoddjj].'|'.$detalle[mesddjj].'|'.$detalle[cuil].'|'.$detalle[remundeclarada].'|'.$detalle[adherentes];
-																fwrite($punterodetalle, $registrodetalle."\n");
-															}
-															fclose($punterodetalle);
-															
-															$sqlLoadDetalleDJ = "LOAD DATA LOCAL INFILE '$archivo_detalle' REPLACE INTO TABLE detddjjospim FIELDS TERMINATED BY '|' LINES TERMINATED BY '\n'";
-															$resLoadDetalleDJ = mysqli_query($linkid, $sqlLoadDetalleDJ);
-															if (!$resLoadDetalleDJ) {
-																$tituloform = "ERROR";
-																$mensaje = 'La carga de los registros seleccionados para el detalle de DDJJ (DETDDJJOSPIM) FALLO.';
-															} else {
-																$sqlFiscalizDJ = "SELECT cuit, anoddjj, mesddjj, secuenciapresentacion FROM afipddjj WHERE nrodisco = '$proximonro' GROUP BY cuit, anoddjj, mesddjj, secuenciapresentacion ORDER BY cuit, anoddjj, mesddjj, secuenciapresentacion";
-																$resFiscalizDJ = $dbl->query($sqlFiscalizDJ);
-																if (!$resFiscalizDJ){
-																	$tituloform = "ERROR";
-																	$mensaje = 'El proceso de seleccion de los registros para el agrupamiento de DDJJ de Fiscalizacion FALLO.';
-																} else {
-																	$archivo_fiscali = $destino_ddjj."NOFI".$proximonro.".txt";
-																	$punterofiscali = fopen($archivo_fiscali, 'w') or die("Hubo un error al generar el archivo de agrupamientos de DDJJ para FISCALIZACION");
-																	foreach ($resFiscalizDJ as $fiscaliz){
-																		$sqlTotalesFisDJ = "SELECT remundeclarada, adherentes FROM afipddjj WHERE nrodisco = '$proximonro' AND cuit = '$fiscaliz[cuit]' AND anoddjj = '$fiscaliz[anoddjj]' AND mesddjj = '$fiscaliz[mesddjj]' AND secuenciapresentacion = '$fiscaliz[secuenciapresentacion]'";
-																		$resTotalesFisDJ = $dbl->query($sqlTotalesFisDJ);
-																		if (!$resTotalesFisDJ){
-																			$tituloform = "ERROR";
-																			$mensaje = 'El proceso de seleccion de los registros para el agrupamiento de DDJJ de Fiscalizacion FALLO.';
-																		} else {
-																			(int)$cantcuilmenor240=0;
-																			(float)$remucuilmenor1001=0.00;
-																			(int)$cantcuilmenor1001=0;
-																			(float)$remuadhemenor1001=0.00;
-																			(int)$cantadhemenor1001=0;
-																			(float)$remucuilmayor1000=0.00;
-																			(int)$cantcuilmayor1000=0;
-																			(float)$remuadhemayor1000=0.00;
-																			(int)$cantadhemayor1000=0;
-
-																			foreach ($resTotalesFisDJ as $totales){
-																				if($totales[remundeclarada] < 240.00) {
-																					$cantcuilmenor240++;
-																				}
-																				if($totales[remundeclarada] <= 1000.00) {
-																					$remucuilmenor1001=round($remucuilmenor1001,2)+round($totales[remundeclarada],2);
-																					$cantcuilmenor1001++;
-																					if($totales[adherentes]!= 0) {
-																						$remuadhemenor1001=round($remuadhemenor1001,2)+round($totales[remundeclarada],2);
-																						$cantadhemenor1001++;
-																					}
-																				}
-																				if($totales[remundeclarada] > 1000.00) {
-																					$remucuilmayor1000=round($remucuilmayor1000,2)+round($totales[remundeclarada],2);
-																					$cantcuilmayor1000++;
-																					if($totales[adherentes]!= 0) {
-																						$remuadhemayor1000=round($remuadhemayor1000,2)+round($totales[remundeclarada],2);
-																						$cantadhemayor1000++;
-																					}
-																				}
-																			}
-																			$registrofiscali = $fiscaliz[cuit].'|'.$fiscaliz[anoddjj].'|'.$fiscaliz[mesddjj].'|'.$cantcuilmenor240.'|'.round($remucuilmenor1001,2).'|'.$cantcuilmenor1001.'|'.round($remuadhemenor1001,2).'|'.$cantadhemenor1001.'|'.round($remucuilmayor1000,2).'|'.$cantcuilmayor1000.'|'.round($remuadhemayor1000,2).'|'.$cantadhemayor1000;
-																			fwrite($punterofiscali, $registrofiscali."\n");
-																		}
-																	}
-																	fclose($punterofiscali);
-																	chmod($archivo_fiscali, 0777);
-																	$sqlLoadFiscaliDJ = "LOAD DATA LOCAL INFILE '$archivo_fiscali' REPLACE INTO TABLE agrufiscalizospim FIELDS TERMINATED BY '|' LINES TERMINATED BY '\n'";
-																	$resLoadFiscaliDJ =  mysqli_query($linkid, $sqlLoadFiscaliDJ);
-																	mysqli_close($linkid);
-																	if (!$resLoadFiscaliDJ) {
-																		$tituloform = "ERROR";
-																		$mensaje = 'La carga de los registros para el agrupamiento de DDJJ de Fiscalizacion (AGRUFISCALIZOSPIM) FALLO.';
-																	} else {
-																		if(!unlink($archivo_descom)) {
-																			$tituloaviso = "AVISO";
-																			$aviso = 'El archivo descomprimido desde el ZIP NO pudo ser eliminado.';
-																		}
-	
-																		rename($archivo_ddjj,$destino_ddjj."DDJJ_NOMINA_OS111001_Q".$nombrearchivo.".zip");
-	
-																		$sqlAddMensaje = "INSERT INTO afipmensajes (nromensaje, fechaemailafip, cuentaderecepcion, tipoarchivo, nrodisco) VALUES (:nromensaje,:fechaemailafip,:cuentaderecepcion,:tipoarchivo,:nrodisco)";
-																		$resAddMensaje = $dbl->prepare($sqlAddMensaje);
-																		if($resAddMensaje->execute(array(':nromensaje' => $nromensaje, ':fechaemailafip' => $fechamensaje, ':cuentaderecepcion' => 'afipnomina@ospim.com.ar', ':tipoarchivo' => 'NODJ', ':nrodisco' => $proximonro))) {
-																			$tituloaviso = "AVISO";
-																			$aviso = 'El mensaje de AFIP ha sido marcado como leido.';
-																		} else {
-																			$tituloaviso = "AVISO";
-																			$aviso = 'El mensaje de AFIP NO ha podido ser marcado como leido.';
-																		}
-	
-																		$tituloform = "PROCESO EXITOSO";
-																		$mensaje = 'El PROCESAMIENTO del archivo de nominas por DDJJ ha sido EXITOSO.';
-																	}
-																}
-															}
-														}
-													}
+												if(!unlink($archivo_descom)) {
+													$tituloaviso = "AVISO";
+													$aviso = 'El archivo descomprimido desde el ZIP NO pudo ser eliminado.';
 												}
+
+												rename($archivo_padron,$destino_padron."DDJJ_PADRON_OS111001.zip");
+	
+												$sqlAddMensaje = "INSERT INTO afipmensajes (nromensaje, fechaemailafip, cuentaderecepcion, tipoarchivo, nrodisco) VALUES (:nromensaje,:fechaemailafip,:cuentaderecepcion,:tipoarchivo,:nrodisco)";
+												$resAddMensaje = $dbl->prepare($sqlAddMensaje);
+												if($resAddMensaje->execute(array(':nromensaje' => $nromensaje, ':fechaemailafip' => $fechamensaje, ':cuentaderecepcion' => 'afippadron@ospim.com.ar', ':tipoarchivo' => 'PADJ', ':nrodisco' => $proximonro))) {
+													$tituloaviso = "AVISO";
+													$aviso = 'El mensaje de AFIP ha sido marcado como leido.';
+												} else {
+													$tituloaviso = "AVISO";
+													$aviso = 'El mensaje de AFIP NO ha podido ser marcado como leido.';
+												}
+	
+												$tituloform = "PROCESO EXITOSO";
+												$mensaje = 'El PROCESAMIENTO del archivo de Padron por DDJJ ha sido EXITOSO.';
 											}
 										} else {
 											$tituloform = "ERROR";
-											$mensaje = 'La identificacion del archivo de DDJJ por Nro. de disco FALLO.';
+											$mensaje = 'La identificacion del archivo de Padron por DDJJ por Nro. de disco FALLO.';
 										}
 										$dbl->commit();
 									} catch (PDOException $e) {
@@ -280,7 +175,7 @@ if($noHayPadron) {
 				else {
 					if($i == 0) {
 						$tituloform = "ERROR";
-						$mensaje = 'El archivo encontrado NO corresponde a una transferencia de APORTES.';
+						$mensaje = 'El archivo encontrado NO corresponde a un padron por DDJJ.';
 						if(unlink($archivo_descom)) {
 							$tituloaviso = "AVISO";
 							$aviso = 'El archivo descomprimido desde el ZIP SE elimino correctamente.';
@@ -295,7 +190,7 @@ if($noHayPadron) {
 			}
 		} else {
 			$tituloform = "ERROR";
-			$mensaje = 'NO se pudo extraer archivo de DDJJ desde el ZIP.';
+			$mensaje = 'NO se pudo extraer archivo de Padron por DDJJ desde el ZIP.';
 		}
 	}
 }
---
+++ ospim/sistemas/afip/detallesPadrones.php
@@ -0,0 +1,76 @@
+<?php $libPath = $_SERVER['DOCUMENT_ROOT']."/lib/";
+include($libPath."controlSessionOspimSistemas.php"); 
+include($libPath."fechas.php");
+$fechamensaje=$_GET['fechaMens'];
+$nromensaje=$_GET['nroMail'];
+$ctamensaje='afippadron@ospim.com.ar';
+
+$sqlBuscaMensaje = "SELECT * FROM afipmensajes WHERE nromensaje = '$nromensaje' AND fechaemailafip = '$fechamensaje' AND cuentaderecepcion = '$ctamensaje'";
+$resBuscaMensaje = mysql_query($sqlBuscaMensaje,$db);
+$canBuscaMensaje = mysql_num_rows($resBuscaMensaje);
+if($canBuscaMensaje!=0) {
+	$rowBuscaMensaje = mysql_fetch_array($resBuscaMensaje);
+	$nrodisco = $rowBuscaMensaje['nrodisco'];
+
+	if(strcmp($rowBuscaMensaje['tipoarchivo'],"PADJ")==0) {
+		$tituloform = 'Resultados de Procesamiento Padrones por DDJJ - Archivo Nro: '.$nrodisco;
+		$sqlBuscaArchivo = "SELECT * FROM padronesddjj WHERE nrodisco = '$nrodisco'";
+		$resBuscaArchivo = mysql_query($sqlBuscaArchivo,$db);
+		$canBuscaArchivo = mysql_num_rows($resBuscaArchivo);
+		if($canBuscaArchivo!=0) {
+			$rowBuscaArchivo = mysql_fetch_array($resBuscaArchivo);
+		}
+		else {
+		}
+	}
+} else {
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>.: Detalle Procesamiento Transferencias AFIP :.</title>
+</head>
+<style>
+A:link {text-decoration: none;color:#0033FF}
+A:visited {text-decoration: none}
+A:hover {text-decoration: none;color:#00FFFF }
+.Estilo2 {
+	font-weight: bold;
+	font-size: 18px;
+}
+</style>
+<body bgcolor="#CCCCCC">
+<div align="center">
+	<h2><?php echo $tituloform;?></h2>
+</div>
+<table width="1233" border="0">
+  <tr>
+    <td colspan="2"><div align="center"><h3>AFIP</h3></div></td>
+    <td colspan="2"><div align="center"><h3>OSPIM</h3></div></td>
+  </tr>
+  <tr>
+    <td width="425">Fecha del Mensaje :</td>
+    <td width="261"><?php echo $rowBuscaArchivo['fechaemailafip'];?></td>
+    <td width="425">Fecha de Procesamiento :</td>
+    <td width="104"><?php echo $rowBuscaArchivo['fechaprocesoospim'];?></td>
+  </tr>
+  <tr>
+    <td>Total de Registros Informados :</td>
+    <td><?php echo $rowBuscaArchivo['registrosafip'];?></td>
+    <td>Total de Registros Procesados :</td>
+    <td><?php echo $rowBuscaArchivo['registrosprocesoospim'];?></td>
+  </tr>
+  
+  <tr>
+    <td colspan="2">&nbsp;</td>
+    <td>Carpeta de Almacenamiento :</td>
+    <td><?php echo $rowBuscaArchivo['carpetaarchivoospim'];?></td>
+  </tr>
+</table>
+<div align="center">
+	<input type="button" name="imprimir" value="Imprimir" onClick="window.print();" align="right"/>
+</div>
+</body>
+</html>
