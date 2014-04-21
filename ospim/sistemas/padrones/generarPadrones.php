7cf324476da654d56bd5da9d55a2f3b05431b024
tanomartin05 <tanomartin05@gmail.com>
2014-04-21 18:16:22 +0000

    agregado sistema de padrones. NOTA: hay que agregar a mano carpeta padrones para pruebas locales.

---
+++ ospim/sistemas/padrones/generarPadrones.php
@@ -0,0 +1,204 @@
+<?php $libPath = $_SERVER['DOCUMENT_ROOT']."/lib/";
+include($libPath."controlSessionOspimSistemas.php");
+include($libPath."fechas.php"); 
+require_once($libPath."phpExcel/Classes/PHPExcel.php");
+
+$maquina = $_SERVER['SERVER_NAME'];
+if(strcmp("localhost",$maquina) == 0) {
+	$direArc = $_SERVER['DOCUMENT_ROOT']."/ospim/sistemas/padrones/archivos/";
+} else {
+	//$direArc="/home/sistemas/Documentos/Liquidaciones/Preliquidaciones/PruebasLiq/".$nombreArc;
+}
+
+var_dump($_POST);
+print("CARPETA DE GUARDADO: ".$direArc."<br>");
+$periodo = explode('-',$_POST['periodo']);
+$mes = $periodo[0];
+$anio = $periodo[1];
+print("MES A REALIZAR: ".$mes." - ".$anio."<br><br>");
+$finalFor = sizeof($_POST) - 2;
+$datos = array_values($_POST);
+
+for ($f = 0; $f < $finalFor; $f++) {
+	$presta = $datos[$f];
+	$nomExcelTitu = $presta."T".$mes.$anio.".xls";
+	$direCompletaTitulares = $direArc.$nomExcelTitu;
+	unlink($direCompletaTitulares);
+	
+	$nomExcelFami = $presta."F".$mes.$anio.".xls";
+	$direCompletaFamiliares = $direArc.$nomExcelFami;
+	unlink($direCompletaFamiliares);
+	
+	$nomTxtTeso = $presta."D".$mes.$anio.".txt";
+
+	try {
+		$objPHPExcel = new PHPExcel();
+		$objPHPExcel->getProperties()->setCreator($_SESSION['usuario'])
+									 ->setLastModifiedBy($_SESSION['usuario'])
+									 ->setTitle($nomExcelTitu)
+									 ->setSubject("Titulares Padron");
+		$objPHPExcel->getActiveSheet()->setTitle($nomExcelTitu);
+		$objPHPExcel->setActiveSheetIndex(0);
+		$fechagenera=date("d/m/Y"); 
+		$objPHPExcel->getActiveSheet()->getPageSetup()->setOrientation(PHPExcel_Worksheet_PageSetup::ORIENTATION_LANDSCAPE);
+		$objPHPExcel->getActiveSheet()->getPageSetup()->setPaperSize(PHPExcel_Worksheet_PageSetup::PAPERSIZE_LEGAL);	
+		$objPHPExcel->getActiveSheet()->getPageSetup()->setHorizontalCentered(true);
+		$objPHPExcel->getActiveSheet()->getPageSetup()->setVerticalCentered(false);
+		
+		$objPHPExcel->getActiveSheet()->getColumnDimension('A')->setWidth(15);
+		$objPHPExcel->getActiveSheet()->setCellValue('A1', 'Nro. AFiliado');
+		$objPHPExcel->getActiveSheet()->getColumnDimension('B')->setWidth(15);
+		$objPHPExcel->getActiveSheet()->setCellValue('B1', 'Nombre y Apellido');
+		$objPHPExcel->getActiveSheet()->getColumnDimension('C')->setWidth(15);
+		$objPHPExcel->getActiveSheet()->setCellValue('C1', 'Tipo Documento');
+		$objPHPExcel->getActiveSheet()->getColumnDimension('D')->setWidth(15);
+		$objPHPExcel->getActiveSheet()->setCellValue('D1', 'Nro. Documento');
+		$objPHPExcel->getActiveSheet()->getColumnDimension('E')->setWidth(15);
+		$objPHPExcel->getActiveSheet()->setCellValue('E1', 'Fecha Nacimiento');
+		$objPHPExcel->getActiveSheet()->getColumnDimension('F')->setWidth(15);
+		$objPHPExcel->getActiveSheet()->setCellValue('F1', 'Sexo');
+		$objPHPExcel->getActiveSheet()->getColumnDimension('G')->setWidth(15);
+		$objPHPExcel->getActiveSheet()->setCellValue('G1', 'Direccion');
+		$objPHPExcel->getActiveSheet()->getColumnDimension('H')->setWidth(15);
+		$objPHPExcel->getActiveSheet()->setCellValue('H1', 'Localidad');
+		$objPHPExcel->getActiveSheet()->getColumnDimension('I')->setWidth(15);
+		$objPHPExcel->getActiveSheet()->setCellValue('I1', 'Provincia');
+		$objPHPExcel->getActiveSheet()->getColumnDimension('J')->setWidth(15);
+		$objPHPExcel->getActiveSheet()->setCellValue('J1', 'Telefono');
+		$objPHPExcel->getActiveSheet()->getColumnDimension('K')->setWidth(15);
+		$objPHPExcel->getActiveSheet()->setCellValue('K1', 'Cod. Delegacion');
+		$objPHPExcel->getActiveSheet()->getColumnDimension('L')->setWidth(15);
+		$objPHPExcel->getActiveSheet()->setCellValue('L1', 'CUIT empresa');
+		$objPHPExcel->getActiveSheet()->getColumnDimension('M')->setWidth(15);
+		$objPHPExcel->getActiveSheet()->setCellValue('M1', 'Razon Social');
+					
+		$objPHPExcel->getDefaultStyle()->getFont()->setName('Arial');
+		$objPHPExcel->getDefaultStyle()->getFont()->setSize(12);
+			
+		$objPHPExcel->getActiveSheet()->getStyle('A1:M1')->getFont()->setBold(true);
+		$objPHPExcel->getActiveSheet()->getStyle('A1:M1')->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID);
+		$objPHPExcel->getActiveSheet()->getStyle('A1:M1')->getFill()->getStartColor()->setARGB('FF808080');
+		$objPHPExcel->getActiveSheet()->getStyle('A1:M1')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+		
+		$fila=1;
+		$filaFamilia=0;	
+		$objPHPExcel->getDefaultStyle()->getFont()->setSize(10);
+		$cuerpoFamilia = array();
+		
+		$sqlDelega = "SELECT * FROM prestadelega where codigopresta = $presta";
+		print($sqlDelega."<br>");
+		$resPresta = mysql_query($sqlDelega,$db);
+		$totalizador = array();
+		while($rowPresta = mysql_fetch_array($resPresta)) { 
+			$totalTituXDelega = 0;
+			$totalFamiXDelega = 0;
+			$delega = $rowPresta['codidelega'];
+			
+			$sqlTitulares = "SELECT t.*, l.nomlocali, p.descrip as nomprovin, e.nombre as nomempresa FROM titulares t, localidades l, provincia p, empresas e where t.codidelega = $delega and t.codlocali = l.codlocali and t.codprovin = p.codprovin and t.cuitempresa = e.cuit";
+			print($sqlTitulares."<br>");
+			$resTitulares = mysql_query($sqlTitulares, $db);	
+			
+			while($rowTitulares = mysql_fetch_array($resTitulares)) {
+				$fila++;
+				$objPHPExcel->getActiveSheet()->setCellValue('A'.$fila, $rowTitulares['nroafiliado']);
+				$objPHPExcel->getActiveSheet()->setCellValue('B'.$fila, $rowTitulares['apellidoynombre']);
+				$objPHPExcel->getActiveSheet()->setCellValue('C'.$fila, $rowTitulares['tipodocumento']);
+				$objPHPExcel->getActiveSheet()->setCellValue('D'.$fila, $rowTitulares['nrodocumento']);
+				$objPHPExcel->getActiveSheet()->setCellValue('E'.$fila, invertirFecha($rowTitulares['fechanacimiento']));
+				$objPHPExcel->getActiveSheet()->setCellValue('F'.$fila, $rowTitulares['sexo']);
+				$objPHPExcel->getActiveSheet()->setCellValue('G'.$fila, $rowTitulares['domicilio']);
+				$objPHPExcel->getActiveSheet()->setCellValue('H'.$fila, $rowTitulares['nomlocali']);
+				$objPHPExcel->getActiveSheet()->setCellValue('I'.$fila, $rowTitulares['nomprovin']);
+				$objPHPExcel->getActiveSheet()->setCellValue('J'.$fila, $rowTitulares['telefono']);
+				$objPHPExcel->getActiveSheet()->setCellValue('K'.$fila, $rowTitulares['codidelega']);
+				$objPHPExcel->getActiveSheet()->setCellValue('L'.$fila, $rowTitulares['cuitempresa']);
+				$objPHPExcel->getActiveSheet()->setCellValue('M'.$fila, $rowTitulares['nomempresa']);
+				$totalTituXDelega++;
+			
+				$nroafil = $rowTitulares['nroafiliado'];
+				$resFamiliares = mysql_query("SELECT f.*, p.descrip as parentezco FROM familiares f, parentesco p where f.nroafiliado = $nroafil and f.tipoparentesco = p.codparent",$db);
+				while($rowFamiliares = mysql_fetch_array($resFamiliares)) {
+					$cuerpoFamilia[$filaFamilia] = array('nroafil' => $rowFamiliares['nroafiliado'], 'parentezco' => $rowFamiliares['parentezco'], 'nombre' => $rowFamiliares['apellidoynombre'], 'tipdoc' => $rowFamiliares['tipodocumento'], 'numdoc' => $rowFamiliares['nrodocumento'], 'fecnac' => invertirFecha($rowFamiliares['fechanacimiento']), 'sexo' => $rowFamiliares['sexo']);
+					$filaFamilia++;
+					$totalFamiXDelega++;
+				}
+			}
+			$totalizador[$delega] = array('delega' => $delega, 'tottit' => $totalTituXDelega, 'totfam' => $totalFamiXDelega);
+		}
+		$totalTitulares = $fila - 1;
+			
+		$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+		$objWriter->save($direCompletaTitulares);
+		$objPHPExcel->disconnectWorksheets();
+		unset($objWriter, $objPHPExcel);
+			
+		//ARCHIVO FAMILIARES
+		$objPHPExcel = new PHPExcel();
+		$objPHPExcel->getProperties()->setCreator($_SESSION['usuario'])
+									 ->setLastModifiedBy($_SESSION['usuario'])
+									 ->setTitle($nomExcelFami)
+									 ->setSubject("Familares Padron");
+		$objPHPExcel->getActiveSheet()->setTitle($nomExcelFami);
+		$objPHPExcel->setActiveSheetIndex(0);
+		$fechagenera=date("d/m/Y"); 
+		$objPHPExcel->getActiveSheet()->getPageSetup()->setOrientation(PHPExcel_Worksheet_PageSetup::ORIENTATION_LANDSCAPE);
+		$objPHPExcel->getActiveSheet()->getPageSetup()->setPaperSize(PHPExcel_Worksheet_PageSetup::PAPERSIZE_LEGAL);	
+		$objPHPExcel->getActiveSheet()->getPageSetup()->setHorizontalCentered(true);
+		$objPHPExcel->getActiveSheet()->getPageSetup()->setVerticalCentered(false);
+			
+		$objPHPExcel->getActiveSheet()->getColumnDimension('A')->setWidth(15);
+		$objPHPExcel->getActiveSheet()->setCellValue('A1', 'Nro. AFiliado');
+		$objPHPExcel->getActiveSheet()->getColumnDimension('B')->setWidth(15);
+		$objPHPExcel->getActiveSheet()->setCellValue('B1', 'Parentezco');
+		$objPHPExcel->getActiveSheet()->getColumnDimension('C')->setWidth(15);
+		$objPHPExcel->getActiveSheet()->setCellValue('C1', 'Nombre y Apellido');
+		$objPHPExcel->getActiveSheet()->getColumnDimension('D')->setWidth(15);
+		$objPHPExcel->getActiveSheet()->setCellValue('D1', 'Tipo Documento');
+		$objPHPExcel->getActiveSheet()->getColumnDimension('E')->setWidth(15);
+		$objPHPExcel->getActiveSheet()->setCellValue('E1', 'Nro. Documento');
+		$objPHPExcel->getActiveSheet()->getColumnDimension('F')->setWidth(15);
+		$objPHPExcel->getActiveSheet()->setCellValue('F1', 'Fecha Nacimiento');
+		$objPHPExcel->getActiveSheet()->getColumnDimension('G')->setWidth(15);
+		$objPHPExcel->getActiveSheet()->setCellValue('G1', 'Sexo');
+			
+		$objPHPExcel->getDefaultStyle()->getFont()->setName('Arial');
+		$objPHPExcel->getDefaultStyle()->getFont()->setSize(12);
+			
+		$objPHPExcel->getActiveSheet()->getStyle('A1:G1')->getFont()->setBold(true);
+		$objPHPExcel->getActiveSheet()->getStyle('A1:G1')->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID);
+		$objPHPExcel->getActiveSheet()->getStyle('A1:G1')->getFill()->getStartColor()->setARGB('FF808080');			
+		$objPHPExcel->getActiveSheet()->getStyle('A1:G1')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+		
+		$fila=1;	
+		$objPHPExcel->getDefaultStyle()->getFont()->setSize(10);
+			
+		foreach($cuerpoFamilia as $familiar) {
+		 	$fila++;
+			$objPHPExcel->getActiveSheet()->setCellValue('A'.$fila, $familiar['nroafil']);
+			$objPHPExcel->getActiveSheet()->setCellValue('B'.$fila, $familiar['parentezco']);
+			$objPHPExcel->getActiveSheet()->setCellValue('C'.$fila, $familiar['nombre']);
+			$objPHPExcel->getActiveSheet()->setCellValue('D'.$fila, $familiar['tipdoc']);
+			$objPHPExcel->getActiveSheet()->setCellValue('E'.$fila, $familiar['numdoc']);
+			$objPHPExcel->getActiveSheet()->setCellValue('F'.$fila, $familiar['fecnac']);
+			$objPHPExcel->getActiveSheet()->setCellValue('G'.$fila, $familiar['sexo']);
+		}
+			
+		$totalFamiliares = $fila - 1;
+			
+		$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+		$objWriter->save($direCompletaFamiliares);
+		$objPHPExcel->disconnectWorksheets();
+		unset($objWriter, $objPHPExcel);
+		
+		//aca tengo que crear el txt de totalizadores...
+		var_dump($totalizador);
+		print("<br>TOTAL ARCHIVOS <br> Total Titulares: $totalTitulares - Total Familiares: $totalFamiliares <br><br>");
+	
+	} catch (PDOException $e) {
+		echo $e->getMessage();
+		$dbh->rollback();
+	}
+}
+
+
+?>
---
+++ ospim/sistemas/padrones/menuPadrones.php
@@ -0,0 +1,93 @@
+<?php $libPath = $_SERVER['DOCUMENT_ROOT']."/lib/";
+include($libPath."controlSessionOspimSistemas.php"); ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>.: Módulo Stock :.</title>
+</head>
+
+<style>
+A:link {text-decoration: none;color:#0033FF}
+A:visited {text-decoration: none}
+A:hover {text-decoration: none;color:#00FFFF }
+.Estilo2 {
+	font-weight: bold;
+	font-size: 18px;
+}
+.Estilo7 {font-weight: bold}
+</style>
+<script src="/lib/jquery.js" type="text/javascript"></script>
+<script src="/lib/jquery.blockUI.js" type="text/javascript"></script>
+<script type="text/javascript">
+function validar(formulario) {
+	if(	!document.getElementById("001").checked && !document.getElementById("002").checked && 
+		!document.getElementById("003").checked && !document.getElementById("004").checked &&
+		!document.getElementById("005").checked && !document.getElementById("006").checked && 
+		!document.getElementById("007").checked && !document.getElementById("008").checked) {
+		alert("Debe Seleccionar por lo menos un Prestador");
+		return(false);
+	}
+	if (formulario.periodo.value == 0) {
+		alert("Debe Seleccionar un Período");
+		return(false);
+	}
+	$.blockUI({ message: "<h1>Generando Padrones<br>Aguarde por favor...</h1>" });
+	return true;
+}
+
+</script>
+
+<body bgcolor="#CCCCCC">
+<div align="center">
+  <p><span style="text-align:center">
+    <input type="reset" name="volver" value="Volver" onclick="location.href = '../menuSistemas.php'" align="center"/>
+  </span></p>
+  <p><span class="Estilo2">Men&uacute; Padrones</span></p>
+  <form id="form1" name="form1" method="post" action="generarPadrones.php" onsubmit="return validar(this)">
+    <p><strong>Seleccionar Prestadore</strong>s</p>
+    <table width="400" border="1">
+	<?php 
+		$resPresta = mysql_query("SELECT * FROM prestadores", $db);
+		while($rowPresta = mysql_fetch_array($resPresta)) { 
+			echo "<tr>";
+			$codigo = $rowPresta['codigo'];
+			$nombre = $rowPresta['nombre'];
+			echo "<td><div align='left'>$codigo - $nombre</div></td>";
+			echo "<td><input type='checkbox' id=$codigo name=$codigo value=$codigo></td>";
+			echo "</tr>";
+		} 
+	?>  
+    </table>  
+    <p class="Estilo7">Periodo 
+      <label>
+		  <?php 
+		  $mes = date("n");
+		  $anio = date("Y");
+		  print("<select name='periodo' id='periodo' name='periodo'>");
+		  print("<option value=0> Seleccione Periodo </option>");
+		  if ($mes == 1) {
+			$aniopasado = date("Y") - 1;
+			$mespasado = 12;
+			print("<option value=$mespasado-$aniopasado>$mespasado - $aniopasado </option>");	
+		  }
+		  if ($mes > 2) {
+		  	$inicio = $mes - 2;
+		  }
+		  for ($i = $inicio; $i < $mes; $i++) {
+		  	print("<option value=$i-$anio>$i-$anio</option>");	
+		  }	
+		  print("</select>");
+		  ?>
+      </label>
+    </p>
+    <p>
+      <label>
+      <input type="submit" name="Submit" value="Generar Padrones" />
+      </label>
+    </p>
+  </form>
+  </div>
+</body>
+</html>
