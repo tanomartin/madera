b615476a9d247f0b834869f919769aff282708c6
tanomartin05 <tanomartin05@gmail.com>
2014-09-12 14:21:41 +0000

    agregado de menues de nomencladores

--- ospim/auditoria/nomenclador/menuNomenclador.php
+++ ospim/auditoria/nomenclador/menuNomenclador.php
@@ -5,7 +5,7 @@ include($libPath."controlSessionOspim.php"); ?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>.: M&oacute;dulo Tesoreria OSPIM :.</title>
+<title>.: Modulo Nomencladores :.</title>
 </head>
 <style>
 A:link {text-decoration: none;color:#0033FF}
@@ -22,18 +22,15 @@ A:hover {text-decoration: none;color:#00FFFF }
   <p>
     <input type="reset" name="volver" value="Volver" onclick="location.href = '../menuAuditoria.php'" align="center"/>
   </p>
-  <p><span class="Estilo2">Men&uacute; Prestadores </span></p>
-  <table width="600" border="3">
+  <p><span class="Estilo2">Men&uacute; Nomencladores </span></p>
+  <table width="420" border="3">
     <tr>
-	  <td width="200"><p align="center">A.B.M.C.</p>
-          <p align="center"><a class="enlace" href="abm/moduloAbmPrestadores.php"><img src="img/prestadores.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+	  <td width="200"><p align="center">Nacional</p>
+          <p align="center"><a class="enlace" href="nacional/menuNacional.php"><img src="img/nacional.png" width="90" height="90" border="0" alt="enviar"/></a></p>
         <p align="center">&nbsp;</p></td>
-      <td width="200"><p align="center">Control C&aacute;pitas</p>
-          <p align="center"><a class="enlace" href="capitas/controlCapitas.php"><img src="img/padrones.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+      <td width="200"><p align="center">No Nomenclado </p>
+          <p align="center"><a class="enlace" href="nonomenclado/menuNoNomenclado.php"><img src="img/nonomenclado.png" width="90" height="90" border="0" alt="enviar"/></a></p>
         <p align="center">&nbsp;</p></td>
-	   <td width="200"><p align="center">Facturación </p>
-          <p align="center"><a class="enlace" href="#"><img src="img/factura.png" width="119" height="92" border="0" alt="enviar"/></a></p>
-        <p align="center">&nbsp;</p></td>		
     </tr>
   </table>
 </div>
---
+++ ospim/auditoria/nomenclador/nacional/menuNacional.php
@@ -0,0 +1,38 @@
+<?php $libPath = $_SERVER['DOCUMENT_ROOT']."/lib/";
+include($libPath."controlSessionOspim.php"); ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>.: Menu Nomenclador Nacional :.</title>
+</head>
+<style>
+A:link {text-decoration: none;color:#0033FF}
+A:visited {text-decoration: none}
+A:hover {text-decoration: none;color:#00FFFF }
+.Estilo2 {
+	font-weight: bold;
+	font-size: 18px;
+}
+</style>
+
+<body bgcolor="#CCCCCC">
+<div align="center">
+  <p>
+    <input type="reset" name="volver" value="Volver" onclick="location.href = '../menuNomenclador.php'" align="center"/>
+  </p>
+  <p><span class="Estilo2">Men&uacute; Nomenclador Nacional </span></p>
+  <table width="400" border="3">
+    <tr>
+	  <td width="200"><p align="center">Listador </p>
+          <p align="center"><a class="enlace" href="#"><img src="img/listador.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+        <p align="center">&nbsp;</p></td>
+      <td width="200"><p align="center">Buscador</p>
+      <p align="center"><a class="enlace" href="#"><img src="img/buscar.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+      <p align="center">&nbsp;</p></td>
+    </tr>
+  </table>
+</div>
+</body>
+</html>
