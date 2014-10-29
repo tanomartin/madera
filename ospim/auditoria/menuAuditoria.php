ac82242588998446bf6aab969885aa70ecfbcf07
tanomartin05 <tanomartin05@gmail.com>
2014-10-29 16:16:38 +0000

    baja nomenclador

--- ospim/auditoria/menuAuditoria.php
+++ ospim/auditoria/menuAuditoria.php
@@ -29,7 +29,7 @@ A:hover {text-decoration: none;color:#00FFFF }
           <p align="center"><a class="enlace" href="autorizaciones/moduloAutorizaciones.php"><img src="img/auditoria.png" width="90" height="90" border="0" alt="enviar"/></a></p>
         <p align="center">&nbsp;</p></td>
 		<td width="200"><p align="center">Nomencladores</p>
-          <p align="center"><a class="enlace" href="nomenclador/menuNomenclador.php"><img src="img/nomenclador.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+          <p align="center"><a class="enlace" href="#"><img src="img/nomenclador.png" width="90" height="90" border="0" alt="enviar"/></a></p>
         <p align="center">&nbsp;</p></td>
     </tr>
   </table>
