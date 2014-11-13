ee118cc6b35e279d6a0d19fa69524e1bf3213a1f
tanomartin05 <tanomartin05@gmail.com>
2014-11-13 15:30:58 +0000

    agregado modulo de desempleo

---
+++ ospim/sistemas/desempleo/menuDesempleo.php
@@ -0,0 +1,123 @@
+<?php $libPath = $_SERVER['DOCUMENT_ROOT']."/lib/";
+include($libPath."controlSessionOspim.php"); 
+
+function formatoPerido($per) {
+	if ($per == 1) {
+		return "01";
+	}
+	if ($per == 2) {
+		return "02";
+	}
+	if ($per == 3) {
+		return "03";
+	}
+	if ($per == 4) {
+		return "04";
+	}
+	if ($per == 5) {
+		return "05";
+	}
+	if (($per == 6) || ($per == -6)) {
+		return "06";
+	}
+	if (($per == 7) || ($per == -5)) {
+		return "07";
+	}
+	if (($per == 8) || ($per == -4)) {
+		return "08";
+	}
+	if (($per == 9) || ($per == -3)) {
+		return "09";
+	}
+	if (($per == 10) || ($per == -2)) {
+		return "10";
+	}
+	if (($per == 11) || ($per == -1)) {
+		return "11";
+	}
+	if (($per == 12) || ($per == 0)){
+		return "12";
+	}
+}
+
+$dia=date("j");
+$mes=date("m");
+$anio=date("Y");
+$inicio=0;
+$fin=2;
+
+for ( $i = $inicio ; $i <= $fin ; $i++) {
+	$perAux=$mes - $i;
+	if ($perAux <= 0) {
+		$anioArc[$i]=$anio-1;
+		$mesArc[$i]=formatoPerido($perAux);
+	}
+	else {
+		$anioArc[$i]=$anio;
+		$mesArc[$i]=formatoPerido($perAux);
+	}
+}
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>.: Subida Archivo A.N.S.E.S. :.</title>
+<style>
+A:link {text-decoration: none;color:#0033FF}
+A:visited {text-decoration: none}
+A:hover {text-decoration: none;color:#00FFFF }
+.Estilo2 {
+	font-weight: bold;
+	font-size: 18px;
+}
+</style>
+<style type="text/css" media="print">
+.nover {display:none}
+</style>
+</head>
+
+<script src="/lib/jquery.js" type="text/javascript"></script>
+<script src="/lib/jquery.blockUI.js" type="text/javascript"></script>
+<script language="javascript" type="text/javascript">
+
+function validar(formulario) {
+	var periodo = formulario.periodo.value;
+	if (periodo == 0) {
+		alert("Debe elegir un periodo para subir el archivo");
+		return false;
+	}
+	$.blockUI({ message: "<h1>Subiendo Archivo de Desempleo<br>Aguarde por favor...</h1>" });
+	return true;
+}
+
+</script>
+
+<body bgcolor="#CCCCCC">
+<div align="center">
+  <p><span style="text-align:center"><input class="nover" type="reset" name="volver" value="Volver" onclick="location.href = '../menuSistemas.php'" align="center"/></span></p>
+  <p class="Estilo2">Subida de Archivo de Desempleo de A.N.S.E.S.</p>
+  <p class="nover"><b>Seleccione Per&iacute;odo</b></p>
+  
+  <form name="seleccionPeriodo" action="subidaArchivoDesempleo.php" onsubmit="return validar(this)" method="POST">	 
+	  <?php if (isset($_GET['existe'])) { ?><p><div style="color:#FF0000"><b>Período "<?php echo substr($_GET['existe'],4,2)."-".substr($_GET['existe'],0,4) ?>" ya procesado</b></div></p>  <?php } ?>
+	  <?php if (isset($_GET['nocarpeta'])) { ?><p><div style="color:#FF0000"><b>No existe la Carpeta "<?php echo $_GET['nocarpeta']?>"</b></div></p>  <?php } ?>
+	  <?php if (isset($_GET['noexiste'])) { ?><p><div style="color:#FF0000"><b>No existe el archivo "<?php echo $_GET['noexiste']?>" del periodo "<?php echo substr($_GET['carpeta'],4,2)."-".substr($_GET['carpeta'],0,4) ?>"</b></div></p>  <?php } ?>
+	  <select class="nover" name="periodo" id="periodo">
+	  <option name="periodo" id="periodo" selected="selected" value=0> Seleccione Periodo </option>
+		  <?php 
+			for ($i=$inicio;$i<=$fin;$i++){
+				$valor = $anioArc[$i].$mesArc[$i];
+				$nombre = $mesArc[$i]."-".$anioArc[$i];
+				print("<option value=$valor>$nombre</option>");	
+			}	
+		  ?>
+	  </select>
+  <p><input class="nover" type="submit" name="submit" value="Subir" /></p>
+  </form>
+  
+</div>
+</body>
+</html>
---
+++ ospim/sistemas/desempleo/subidaArchivoDesempleo.php
@@ -0,0 +1,139 @@
+<?php $libPath = $_SERVER['DOCUMENT_ROOT']."/lib/";
+include($libPath."controlSessionOspim.php"); 
+include($libPath."claves.php");
+
+$maquina = $_SERVER['SERVER_NAME'];
+
+$carpetaMes = $_POST['periodo'];
+$nombreArcProc = "DE".$carpetaMes.".txt";
+$nombreArc = "Desempleo.txt";
+
+if(strcmp("localhost",$maquina) == 0) {
+	$direDirectorio = $_SERVER['DOCUMENT_ROOT']."/ospim/sistemas/desempleo/Desempleo/";
+} else {
+	$direDirectorio="/home/sistemas/Documentos/Repositorio/DescargasSSS/Desempleo/";
+}
+
+$directorioMes = $direDirectorio.$carpetaMes;
+$fileProcDirectorio = $directorioMes."/".$nombreArcProc;
+$fileDirectorio = $directorioMes."/".$nombreArc;
+
+if (file_exists($fileProcDirectorio)){
+	$pagina = "menuDesempleo.php?existe=$carpetaMes";
+	Header("Location: $pagina"); 
+	exit();
+} else { 
+	if (!file_exists($directorioMes)) {
+		$pagina = "menuDesempleo.php?nocarpeta=$carpetaMes";
+		Header("Location: $pagina"); 
+		exit();
+	} 
+	if (!file_exists($fileDirectorio)) {
+		$pagina = "menuDesempleo.php?noexiste=$nombreArc&carpeta=$carpetaMes";
+		Header("Location: $pagina"); 
+		exit();
+	}
+}
+
+$fp = fopen($fileDirectorio, "r");
+$lineasNuevoArchivo = array();
+$i = 1;
+while(!feof($fp)) {
+	$linea = fgets($fp);
+	if (strlen($linea) > 0) {
+		
+		$campos = explode("|",$linea);
+		
+		$fechaCobro = $campos[9];
+		$fechaCobro = substr($fechaCobro,6,4).substr($fechaCobro,3,2).substr($fechaCobro,0,2);
+		$campos[9] = $fechaCobro;
+		$mesFinRelacion = substr($campos[12],2,2);
+		$anoFinRealcion = substr($campos[12],0,2);
+		if ($anoFinRealcion == 0) { $anoFinRealcion  = "0000"; }
+		if ($anoFinRealcion > 0 && $anoFinRealcion < 50) { $anoFinRealcion  = 2000 + $anoFinRealcion; }
+		if ($anoFinRealcion > 49 && $anoFinRealcion < 99) { $anoFinRealcion  = 1900 + $anoFinRealcion; }
+		$campos[16] = $campos[15];
+		$campos[15] = $campos[14];
+		$campos[14] = $campos[13];
+		$campos[13] = $anoFinRealcion;
+		$campos[12] = $mesFinRelacion;
+
+		$linea = implode("|",$campos);
+		
+		$nuevaLinea = substr($carpetaMes,0,4)."|".substr($carpetaMes,4,2)."|".$i."|".$linea;
+		$lineasNuevoArchivo[$i] = $nuevaLinea;
+		$i++;
+	}
+}
+fclose($fp);
+
+$ar=fopen($fileProcDirectorio,"x") or die("Hubo un error al generar el archivo de desempleo para importar a la base. Por favor cuminiquese con el dpto. de Sistemas");
+foreach($lineasNuevoArchivo as $linea) {
+	fputs($ar,$linea);
+}
+fclose($ar);
+
+$sqlImport = "LOAD DATA INFILE '$fileProcDirectorio' REPLACE INTO TABLE desempleosss FIELDS TERMINATED BY '|' LINES TERMINATED BY '\\n'";
+try {
+	$hostname = $_SESSION['host'];
+	$dbname = $_SESSION['dbname'];
+	$dbh = new PDO("mysql:host=$hostname;dbname=$dbname",$_SESSION['usuario'],$_SESSION['clave']);
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$dbh->beginTransaction();
+	//print($sqlImport);
+	$dbh->exec($sqlImport);
+	$dbh->commit();
+}catch (PDOException $e) {
+	echo $e->getMessage();
+	$dbh->rollback();
+}	
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>.: Resultado subida archivo desempleo de ANSES :.</title>
+</head>
+<style>
+A:link {text-decoration: none;color:#0033FF}
+A:visited {text-decoration: none}
+A:hover {text-decoration: none;color:#00FFFF }
+.Estilo2 {
+	font-weight: bold;
+	font-size: 18px;
+}
+</style>
+
+<body bgcolor="#CCCCCC">
+<div align="center">
+  <p class="Estilo2"><span style="text-align:center">
+    <input type="reset" name="volver" value="Volver" onclick="location.href = 'menuDesempleo.php'" align="center"/>
+  </span></p>
+  <p class="Estilo2">Resultado del proceso de subida de archivos de desempleo de A.N.S.E.S. </p>
+			  <table width="500" border="1" align="center">
+					<tr>
+					  <th>Periodo</th>
+					  <th>Lineas Archivo Original</th>
+					  <th>Cantida de reg. importados</th>
+					</tr>
+			  <?php 
+			  		$mesImportacion = substr($carpetaMes,4,2);
+					$anoImporatacion = substr($carpetaMes,0,4);
+					print("<tr align='center'>");
+					print("<td>".$mesImportacion."-".$anoImporatacion."</td>");
+					print("<td>".sizeof($lineasNuevoArchivo)."</td>");
+					
+					$sqlControlImpo = "SELECT cuilbeneficiario FROM desempleosss where anodesempleo = $anoImporatacion and mesdesempleo = $mesImportacion";
+					$resControlImpo = mysql_query($sqlControlImpo,$db);
+					$cantImportadas = mysql_num_rows($resControlImpo);
+
+					print("<td>".$cantImportadas."</td>");
+					print("</tr>");
+			  ?>
+			</table>  </p>
+	 <p><input type="button" name="imprimir" value="Imprimir" onclick="window.print();" align="center"/></p>
+</div>
+</body>
+</html>
