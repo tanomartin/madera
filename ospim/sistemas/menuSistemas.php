84e869733146ce31c6bcfd44410e6651e764563c
tanomartin05 <tanomartin05@gmail.com>
2014-11-13 15:33:37 +0000

    baja de modulo de bajada de información de aplicativo ddjj de usimra.

--- ospim/sistemas/menuSistemas.php
+++ ospim/sistemas/menuSistemas.php
@@ -26,7 +26,7 @@ A:hover {text-decoration: none;color:#00FFFF }
           <p align="center"><a class="enlace" href="fiscalizacion/menuFiscalizacion.php"><img src="img/fiscalizacion.png" width="90" height="90" border="0" alt="enviar"/></a></p>
         <p align="center">&nbsp;</p></td>
       <td width="200"><p align="center">Aplicativo DDJJ </p>
-      <p align="center"><a class="enlace" href="aplicativoddjj/menuAplicativoddjj.php"><img src="img/aplicativoddjj.png" width="97" height="85" border="0" alt="enviar"/></a></p>
+      <p align="center"><a class="enlace" href="#"><img src="img/aplicativoddjj.png" width="97" height="85" border="0" alt="enviar"/></a></p>
       <p align="center">&nbsp;</p></td>
       <td width="200"><p align="center">Tratamiento A.F.I.P.</p>
         <p align="center"><a class="enlace" href="afip/menuAfip.php"><img src="img/afip.jpg" width="90" height="90" border="0" alt="enviar"/></a></p>
