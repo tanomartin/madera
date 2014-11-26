3ab9707f6faa27cf58ee5d0a62faca4d61594173
tanomartin05 <tanomartin05@gmail.com>
2014-11-26 15:55:46 +0000

    arreglo funciones para tomar capitulos y subcatpiulos.

--- lib/funcionespracticas.php
+++ lib/funcionespracticas.php
@@ -1,22 +1,10 @@
 <?php
 
-function descripcionPractica($codigoPractica, $db) {
+function descripcionPractica($codigoPractica, $tipoPractica, $db) {
 	$codigoArray = explode(".",$codigoPractica);
-	if (sizeof($codigoArray) == 1) {
-		$tipoPractica = 1;
-		$sqlTipoPractica = "SELECT descripcion FROM tipopracticas WHERE id = $tipoPractica";
-		$resTipoPractica = mysql_query($sqlTipoPractica,$db);
-		$rowTipoPractica = mysql_fetch_array($resTipoPractica);
-		$resultado['tipo'] = $rowTipoPractica['descripcion'];
-		$resultado['codigosubcapitulo'] = "";
-		$resultado['subcapitulo'] = "";
-		$resultado['codigocapitulo'] = "";
-		$resultado['capitulo'] = "";	
-	}
-	
 	if (sizeof($codigoArray) == 2) {
 		$codCapitulo = $codigoArray[0];
-		$sqlTipoPractica = "SELECT c.descripcion as cap, t.descripcion as tipoprac FROM tipopracticas t, capitulosdepracticas c WHERE c.codigo = '$codCapitulo' and c.idtipopractica != 2 and c.idtipopractica = t.id";
+		$sqlTipoPractica = "SELECT c.descripcion as cap FROM capitulosdepracticas c WHERE c.codigo = '$codCapitulo' and c.idtipopractica = $tipoPractica";
 		$resTipoPractica = mysql_query($sqlTipoPractica,$db);
 		$rowTipoPractica = mysql_fetch_array($resTipoPractica);
 		$resultado['tipo'] = $rowTipoPractica['tipoprac'];
@@ -26,10 +14,9 @@ function descripcionPractica($codigoPractica, $db) {
 		$resultado['capitulo'] =  $rowTipoPractica['cap'];
 	}
 	if (sizeof($codigoArray) == 3) {
-		$tipoPractica = 2;
 		$codCapitulo = $codigoArray[0];
 		$codSubCapitulo = $codigoArray[1];
-		$sqlTipoPractica = "SELECT c.descripcion as cap, t.descripcion as tipoprac, s.descripcion as subcap FROM tipopracticas t, capitulosdepracticas c, subcapitulosdepracticas s WHERE s.codigo = '$codCapitulo.$codSubCapitulo' and s.idcapitulo = c.id and c.idtipopractica = t.id";
+		$sqlTipoPractica = "SELECT c.descripcion as cap, s.descripcion as subcap FROM capitulosdepracticas c, subcapitulosdepracticas s WHERE s.codigo = '$codCapitulo.$codSubCapitulo' and s.idcapitulo = c.id and c.idtipopractica = $tipoPractica";
 		$resTipoPractica = mysql_query($sqlTipoPractica,$db);
 		$rowTipoPractica = mysql_fetch_array($resTipoPractica);
 		$resultado['tipo'] = $rowTipoPractica['tipoprac'];
