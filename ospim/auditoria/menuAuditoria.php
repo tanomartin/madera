cdd97fccdee45a999508b52dc0553d91e802a6ed
tanomartin05 <tanomartin05@gmail.com>
2015-01-16 14:10:26 +0000

    Agregado de menu informes al menu sur.

--- ospim/auditoria/menuAuditoria.php
+++ ospim/auditoria/menuAuditoria.php
@@ -23,14 +23,23 @@ A:hover {text-decoration: none;color:#00FFFF }
   <table width="600" border="3">
     <tr>
       <td width="200"><p align="center">Prestadores</p>
-          <p align="center"><a class="enlace" href="../moduloNoDisponible.php"><img src="img/prestadores.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+          <p align="center"><a class="enlace" href="prestadores/menuPrestadores.php"><img src="img/prestadores.png" width="90" height="90" border="0" alt="enviar"/></a></p>
         <p align="center">&nbsp;</p></td>
       <td width="200"><p align="center">Autorizaciones</p>
           <p align="center"><a class="enlace" href="autorizaciones/moduloAutorizaciones.php"><img src="img/auditoria.png" width="90" height="90" border="0" alt="enviar"/></a></p>
         <p align="center">&nbsp;</p></td>
 		<td width="200"><p align="center">Nomencladores</p>
-          <p align="center"><a class="enlace" href="../moduloNoDisponible.php"><img src="img/nomenclador.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+          <p align="center"><a class="enlace" href="nomenclador/menuNomenclador.php"><img src="img/nomenclador.png" width="90" height="90" border="0" alt="enviar"/></a></p>
         <p align="center">&nbsp;</p></td>
+    </tr>
+    <tr>
+      <td><p align="center">S.U.R. </p>
+        <p align="center"><a href="sur/menuSur.php"><img src="img/sur.jpg" width="90" height="90" border="0" alt="enviar"/></a></p>
+        <p align="center">&nbsp;</p></td>
+      <td><p align="center">Programa de Prevenci&oacute;n </p>
+      <p align="center"><a href="prevencion/menuProgramaPrevencion.php"><img src="img/prevencion.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+      <p>&nbsp;</p></td>
+      <td>&nbsp;</td>
     </tr>
   </table>
 </div>
