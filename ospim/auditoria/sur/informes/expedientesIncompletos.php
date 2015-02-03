2bf6d1fcfd5a3d18a368d7b456f233f12e8d25ee
tanomartin05 <tanomartin05@gmail.com>
2015-02-03 13:52:22 +0000

    Cambio informe expendientes Incompleto por delegación.

--- ospim/auditoria/sur/informes/expedientesIncompletos.php
+++ ospim/auditoria/sur/informes/expedientesIncompletos.php
@@ -1,8 +1,64 @@
 <?php $libPath = $_SERVER['DOCUMENT_ROOT']."/lib/";
 include($libPath."controlSessionOspim.php"); 
 
-$sqlExpediente = "SELECT e.nroafiliado, e.nroorden FROM discapacitadoexpendiente e WHERE e.completo = 0 ";
-$resExpediente = mysql_query($sqlExpediente,$db);
+if (isset($_POST['delegacion'])) { 
+	$resultado = array();
+	$i = 0;
+	$sqlExpediente = "SELECT e.nroafiliado, e.nroorden FROM discapacitadoexpendiente e WHERE e.completo = 0 ";
+	$resExpediente = mysql_query($sqlExpediente,$db);
+	while ($rowExpediente = mysql_fetch_assoc($resExpediente)) {
+		$arrayDelegacion = explode("-",$_POST['delegacion']);
+		$delegacion = $arrayDelegacion[0];
+		$tipoBeneficiario = "";
+		if ($rowExpediente['nroorden'] == 0) { 
+			$sqlBeneficiario = "SELECT apellidoynombre FROM titulares WHERE nroafiliado = ".$rowExpediente['nroafiliado']." and codidelega =".$delegacion;
+			$resBeneficiario = mysql_query($sqlBeneficiario,$db);	
+			$canBeneficiario = mysql_num_rows($resBeneficiario);
+			if($canBeneficiario == 0) {
+				$sqlBeneficiario = "SELECT apellidoynombre FROM titularesdebaja WHERE nroafiliado = ".$rowExpediente['nroafiliado']." and codidelega =".$delegacion;
+				$resBeneficiario = mysql_query($sqlBeneficiario,$db);	
+				$canBeneficiario = mysql_num_rows($resBeneficiario);
+				if($canBeneficiario != 0) {
+					$rowBeneficiario = mysql_fetch_assoc($resBeneficiario);
+					$resultado[$i] = array('nroafiliado' => $rowExpediente['nroafiliado'], 'nroorden' => 0, 'nombre' => $rowBeneficiario['apellidoynombre'], "tipoBeneficiario" => "TITULAR INACTIVO", "activo" => 0);
+					$i++;
+				}
+			} else {
+				$rowBeneficiario = mysql_fetch_assoc($resBeneficiario);
+				$resultado[$i] = array('nroafiliado' => $rowExpediente['nroafiliado'], 'nroorden' => 0, 'nombre' => $rowBeneficiario['apellidoynombre'], "tipoBeneficiario" => "TITULAR", "activo" => 1);
+				$i++;
+			}
+		} else {
+			$sqlBeneficiario = "SELECT f.apellidoynombre FROM familiares f, titulares t WHERE f.nroafiliado = ".$rowExpediente['nroafiliado']." and f.nroorden = ".$rowExpediente['nroorden']." and f.nroafiliado = t.nroafiliado and t.codidelega = ".$delegacion;
+			$resBeneficiario = mysql_query($sqlBeneficiario,$db);	
+			$canBeneficiario = mysql_num_rows($resBeneficiario);
+			if($canBeneficiario == 0) {
+				$sqlBeneficiario = "SELECT f.apellidoynombre FROM familiaresdebaja f, titulares t WHERE f.nroafiliado = ".$rowExpediente['nroafiliado']." and f.nroorden = ".$rowExpediente['nroorden']." and f.nroafiliado = t.nroafiliado and t.codidelega = ".$delegacion;
+				$resBeneficiario = mysql_query($sqlBeneficiario,$db);	
+				$canBeneficiario = mysql_num_rows($resBeneficiario);
+				if($canBeneficiario == 0) {
+					$sqlBeneficiario = "SELECT f.apellidoynombre FROM familiaresdebaja f, titularesdebaja t WHERE f.nroafiliado = ".$rowExpediente['nroafiliado']." and f.nroorden = ".$rowExpediente['nroorden']." and f.nroafiliado = t.nroafiliado and t.codidelega = ".$delegacion;
+					$resBeneficiario = mysql_query($sqlBeneficiario,$db);	
+					$canBeneficiario = mysql_num_rows($resBeneficiario);
+					if($canBeneficiario != 0) {
+						$rowBeneficiario = mysql_fetch_assoc($resBeneficiario);
+						$resultado[$i] = array('nroafiliado' => $rowExpediente['nroafiliado'], 'nroorden' => $rowExpediente['nroorden'], 'nombre' => $rowBeneficiario['apellidoynombre'], "tipoBeneficiario" => "FAMILIAR INACTIVO", "activo" => 0);
+						$i++;
+					}
+				} else {
+					$rowBeneficiario = mysql_fetch_assoc($resBeneficiario);
+					$resultado[$i] = array('nroafiliado' => $rowExpediente['nroafiliado'], 'nroorden' => $rowExpediente['nroorden'], 'nombre' => $rowBeneficiario['apellidoynombre'], "tipoBeneficiario" => "FAMILIAR INACTIVO", "activo" => 0);
+					$i++;
+				}
+			} else {
+				$rowBeneficiario = mysql_fetch_assoc($resBeneficiario);
+				$resultado[$i] = array('nroafiliado' => $rowExpediente['nroafiliado'], 'nroorden' => $rowExpediente['nroorden'], 'nombre' => $rowBeneficiario['apellidoynombre'], "tipoBeneficiario" => "FAMILIAR", "activo" => 1);
+				$i++;
+			}	
+		} 
+	}
+}
+
 
 ?>
 
@@ -10,7 +66,7 @@ $resExpediente = mysql_query($sqlExpediente,$db);
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>.: Listado De Certificados por Fecha Vto. :.</title>
+<title>.: Listado De Expedientes Incompletos :.</title>
 </head>
 <style>
 A:link {text-decoration: none;color:#0033FF}
@@ -26,16 +82,12 @@ A:hover {text-decoration: none;color:#00FFFF }
 </style>
 
 <script src="/lib/jquery.js" type="text/javascript"></script>
-<script src="/lib/funcionControl.js" type="text/javascript"></script>
-<script src="/lib/jquery.maskedinput.js" type="text/javascript"></script>
+<script src="/lib/jquery.blockUI.js" type="text/javascript"></script>
 <link rel="stylesheet" href="/lib/jquery.tablesorter/themes/theme.blue.css">
 <script src="/lib/jquery.tablesorter/jquery.tablesorter.js"></script>
 <script src="/lib/jquery.tablesorter/jquery.tablesorter.widgets.js"></script>
 <script src="/lib/jquery.tablesorter/addons/pager/jquery.tablesorter.pager.js"></script> 
 <script language="javascript" type="text/javascript">
-jQuery(function($){
-	$("#fechavto").mask("99-99-9999");
-});
 
 	$(function() {
 		$("#tabla")
@@ -57,16 +109,7 @@ jQuery(function($){
 
 
 function validar(formulario) {
-	var fechavto = formulario.fechavto.value;
-	if (fechavto == "") {
-		alert("Debe ingresar un fecha de vencimiento de certificado");
-		return(false)
-	} else {
-		if (!esFechaValida(fechavto)) {
-			alert("La fecha de fecha de vencimiento no es valida");
-			return(false);
-		} 
-	}
+	$.blockUI({ message: "<h1>Generando Listado<br>Aguarde por favor...</h1>" });
 	formulario.Submit.disabled = true;
 	return true;
 }
@@ -81,82 +124,53 @@ function abrirConsulta(dire){
 <body bgcolor="#CCCCCC">
 <div align="center">
   <p><input type="reset" name="volver" value="Volver" class="nover" onclick="location.href = 'moduloInformes.php'" align="center"/></p>
+	<form  name="listadoExpedientes" id="listadoExpedientes" method="post" onSubmit="return validar(this)" action="expedientesIncompletos.php">
 	<p><span class="Estilo2">Expendientes Incompletos </span></p>
-	<table style="text-align:center; width:800px" id="tabla" class="tablesorter" >
-		<thead>
-			<tr>
-				<td>Nro. Afiliado</td>
-				<td>Nombre y Apellido</td>
-				<td class="filter-select" data-placeholder="Seleccione Tipo">Tipo Beneficiario</td>
-				<td>Acciones</td>
-			</tr>
-		</thead>
-		<?php while ($rowExpediente = mysql_fetch_assoc($resExpediente)) {?>
-			<tr>
-				<td><?php echo $rowExpediente['nroafiliado'] ?></td>	
-				<td>
-				<?php 
-						$tipoBeneficiario = "";
-						if ($rowExpediente['nroorden'] == 0) { 
-							$sqlBeneficiario = "SELECT apellidoynombre FROM titulares WHERE nroafiliado = ".$rowExpediente['nroafiliado'];
-							$resBeneficiario = mysql_query($sqlBeneficiario,$db);	
-							$canBeneficiario = mysql_num_rows($resBeneficiario);
-							if($canBeneficiario == 0) {
-								$sqlBeneficiario = "SELECT apellidoynombre FROM titularesdebaja WHERE nroafiliado = ".$rowExpediente['nroafiliado'];
-								$resBeneficiario = mysql_query($sqlBeneficiario,$db);	
-								$canBeneficiario = mysql_num_rows($resBeneficiario);
-								if($canBeneficiario == 0) {
-									echo ('No se encontro el Beneficiario');
-									$tipoBeneficiario = "-";
-								} else {
-									$rowBeneficiario = mysql_fetch_assoc($resBeneficiario);
-									echo ($rowBeneficiario['apellidoynombre']);
-									$tipoBeneficiario = "TITULAR INACTIVO";
-									$activo = 0;
-								}
-							} else {
-								$activo = 1;
-								$rowBeneficiario = mysql_fetch_assoc($resBeneficiario);
-								echo ($rowBeneficiario['apellidoynombre']);
-								$tipoBeneficiario = "TITULAR";
-							}
-					   } else {
-					   		$sqlBeneficiario = "SELECT apellidoynombre FROM familiares WHERE nroafiliado = ".$rowExpediente['nroafiliado']." and nroorden = ".$rowExpediente['nroorden'];
-							$resBeneficiario = mysql_query($sqlBeneficiario,$db);	
-							$canBeneficiario = mysql_num_rows($resBeneficiario);
-							if($canBeneficiario == 0) {
-								$sqlBeneficiario = "SELECT apellidoynombre FROM familiaresdebaja WHERE nroafiliado = ".$rowExpediente['nroafiliado']." and nroorden = ".$rowExpediente['nroorden'];
-								$resBeneficiario = mysql_query($sqlBeneficiario,$db);	
-								$canBeneficiario = mysql_num_rows($resBeneficiario);
-								if($canBeneficiario == 0) {
-									echo ('No se encontro el Beneficiario');
-									$tipoBeneficiario = "-";
-								} else {
-									$rowBeneficiario = mysql_fetch_assoc($resBeneficiario);
-									echo ($rowBeneficiario['apellidoynombre']);
-									$tipoBeneficiario = "FAMILIAR INACTIVO";
-									$activo = 0;
-								}
-							} else {
-								$rowBeneficiario = mysql_fetch_assoc($resBeneficiario);
-								echo ($rowBeneficiario['apellidoynombre']);
-								$tipoBeneficiario = "FAMILIAR";
-								$activo = 1;
-							}	
-					  	 } ?>
-				</td>
-				<td><?php echo $tipoBeneficiario ?></td>		
-				<td><?php if ($canBeneficiario != 0) { ?>
-					<input type='button' name='consultar' value='Consultar' onclick="abrirConsulta('../abm/consultarDiscapacitado.php?nroafiliado=<?php echo $rowExpediente['nroafiliado'] ?>&nroorden=<?php echo $rowExpediente['nroorden'] ?>&activo=<?php echo $activo ?>&nomostrar=1')" /></td>		
-					<?php } ?>
-				</tr>
-		<?php } ?>
-		<tbody>
-		</tbody>
-	</table>
-    <p>
-      <input class="nover" type="button" name="imprimir" value="Imprimir" onclick="window.print();" align="center"/>
-  </p>
+	<table>
+      <tr>
+        <td><div align="left"> <strong>Delegaci&oacute;n</strong>
+                <select name="delegacion" id="delegacion" class="nover">
+                  <option value="0" selected="selected">Seleccione un Valor </option>
+                  <?php 
+							$sqlDele="select codidelega,nombre from delegaciones where codidelega not in (1000,1001,3500)";
+							$resDele= mysql_query($sqlDele,$db);
+							while ($rowDele=mysql_fetch_array($resDele)) { 	?>
+                  <option value="<?php echo $rowDele['codidelega']."-".$rowDele['nombre'] ?>"><?php echo $rowDele['nombre']  ?></option>
+                  <?php } ?>
+                </select>
+        </div></td>
+      </tr>
+    </table>
+    <p> <input type="submit" name="Submit" value="Listar" class="nover"/> </p>
+<?php if (isset($_POST['delegacion'])) { ?>
+		<p><span class="Estilo2">Resultado Delegaci&oacute;n <?php echo $_POST['delegacion'] ?></span> </p>
+<?php 	if (sizeof($resultado) != 0) { ?>
+			<table style="text-align:center; width:800px" id="tabla" class="tablesorter" >
+				<thead>
+					<tr>
+						<td>Nro. Afiliado</td>
+						<td>Nombre y Apellido</td>
+						<td class="filter-select" data-placeholder="Seleccione Tipo">Tipo Beneficiario</td>
+						<td>Acciones</td>
+					</tr>
+				</thead>
+				<tbody>
+				<?php foreach($resultado as $bene) { ?> 
+					<tr>
+						<td><?php echo $bene['nroafiliado'] ?></td>
+						<td><?php echo $bene['nombre'] ?></td>
+						<td><?php echo $bene['tipoBeneficiario'] ?></td>
+						<td><input type='button' name='consultar' value='Consultar' onclick="abrirConsulta('../abm/consultarDiscapacitado.php?nroafiliado=<?php echo $bene['nroafiliado'] ?>&nroorden=<?php echo $bene['nroorden'] ?>&activo=<?php echo $bene['activo'] ?>&nomostrar=1')" /></td>		
+					</tr>
+				<?php } ?>
+				</tbody>
+			</table>
+			<p> <input class="nover" type="button" name="imprimir" value="Imprimir" onclick="window.print();" align="center"/></p>
+ <?php } else {
+			echo "<font color='#FF0000'><b>No existen Expedientes Incompletos</b></font>";
+ 	   }
+  } ?>
+	</form>
 </div>
 </body>
 </html>
