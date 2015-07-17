293dc44e32fcbb0221e9756f7ab912e5068ac44b
tanomartin <tanomartin05@gmail.com>
2015-07-17 10:17:50 -0300

    cambio nosmbres php de estados contables.

--- ospim/contaduria/estadocontable/detalleEstadoContable.php
+++ ospim/contaduria/estadocontable/detalleEstadoContable.php
@@ -215,7 +215,7 @@ try {
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>.: Listado de Titulares por Empresa :.</title>
+<title>.: Detalle Estado Contable :.</title>
 
 <style>
 A:link {
