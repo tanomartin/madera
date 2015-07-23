40c0214dd84c980c2ee58097a40ddc2ffb769d65
tanomartin <tanomartin05@gmail.com>
2015-07-23 10:45:41 -0300

    cambio menu acuerdo

--- usimra/acuerdos/menuAcuerdos.php
+++ usimra/acuerdos/menuAcuerdos.php
@@ -39,10 +39,7 @@ A:hover {text-decoration: none;color:#00FFFF }
       <td><p align="center">Valores al Cobro</p>
       <p align="center"><a href="valores/menuValores.php"><img src="img/valores.png" width="90" height="90" border="0" /></a></p>
       <p align="center">&nbsp;</p></td>
-      <td><p align="center"></p>
-	  	  <p align="center"></p>
-	  	  <p align="center">&nbsp;</p></td>
-      
+      <td><p align="center">&nbsp;</p></td>
     </tr>
   </table>
 </div>
