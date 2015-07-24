1064e6e64fb12c06678b1ea662ea62dc86eb5ef1
tanomartin <tanomartin05@gmail.com>
2015-07-24 11:03:36 -0300

    cierre de liquidacion OSPIM

--- ospim/sistemas/fiscalizacion/cierreLiquidacion.php
+++ ospim/sistemas/fiscalizacion/cierreLiquidacion.php
@@ -1,22 +1,22 @@
 <?php $libPath = $_SERVER['DOCUMENT_ROOT']."/madera/lib/";
 include($libPath."controlSessionOspimSistemas.php"); 
 include($libPath."fechas.php"); 
-include($libPath."ftpZeus.php"); 
-include($libPath."funcionesFTP.php"); 
 
 $archivo = "Seguimiento.txt";
 $archivoUsimra = "SegUSIMRA.txt";
 $maquina = $_SERVER['SERVER_NAME'];
 if(strcmp("localhost",$maquina) == 0) {
-	$direArc = $_SERVER['DOCUMENT_ROOT']."/madera/ospim/sistemas/fiscalizacion/liqui/".$archivo;
-	$direArcUsimra = $_SERVER['DOCUMENT_ROOT']."/madera/ospim/sistemas/fiscalizacion/liqui/".$archivoUsimra;
+	$direArc = "liqui/".$archivo;
+	$direArcUsimra = "liqui/".$archivoUsimra;
 } else {
 	$direArc = "/home/sistemas/Documentos/Liquidaciones/Preliquidaciones/".$archivo;
 	$direArcUsimra = "/home/sistemas/Documentos/Liquidaciones/Preliquidaciones/".$archivoUsimra;
 }
 
+$liqCargadas = array();
+$sqlInserciones = array();
 if (file_exists($direArc)) {
-	$file = fopen($direArc, "r") or exit("Falla al abrir el archivo!");
+	$file = fopen($direArc, "r") or exit("Falla al abrir el archivo OSPIM!");
 	$r = 0;
 	$n = 0;
 	while(!feof($file)) {
@@ -70,50 +70,101 @@ if (file_exists($direArc)) {
 		}
 	}
 	fclose($file);
-	
-	//INSERTAMOS TODO
-	try {
-		$hostname = $_SESSION['host'];
-		$dbname = $_SESSION['dbname'];
-		$dbh = new PDO("mysql:host=$hostname;dbname=$dbname",$_SESSION['usuario'],$_SESSION['clave']);
-		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$dbh->beginTransaction();
-		foreach($sqlInserciones as $sql) {
-			//print($sql."<br>");
-			$dbh->exec($sql);
-		}
-		$dbh->commit();
-		unlink($direArc);
-	}catch (PDOException $e) {
-		echo $e->getMessage();
-		$dbh->rollback();
-	}	
-} else {
-	$liqCargadas = 0;
 }
 
-$liqUsimra = 0;
+$liqCargadasUsimra = array();
+$sqlInsercionesUsimra = array();
 if (file_exists($direArcUsimra)) {
-	$pathZeus = "/home/sistemas/seguimiento";
-	$resultado = SubirArchivo($direArcUsimra, $archivoUsimra, $pathZeus);
-	if ($resultado) {
-		$liqUsimra = 1;
-		//unlink($direArcUsimra);
-	} else {
-		$liqUsimra = 2;
-	}
-} 
+	$file = fopen($direArcUsimra, "r") or exit("Falla al abrir el archivo USIMRA!");
+	$r = 0;
+	$n = 0;
+	while(!feof($file)) {
+		$linea = trim(fgets($file));
+		if (strlen($linea) > 0) {
+			$acuAbs = "";
+			$linea = str_replace('"','',$linea);
+			$campos = explode("|",$linea);
+			$nroreq = (int)$campos[0];
+				
+			$sqlCuit = "SELECT cuit FROM reqfiscalizusimra where nrorequerimiento = $nroreq";
+			$resCuit = mysql_query($sqlCuit,$db);
+			$rowCuit = mysql_fetch_array($resCuit);
+			$cuit = $rowCuit['cuit'];
 
+			$sqlInsercionesUsimra[$r] = "DELETE FROM aculiquiusimra where nrorequerimiento = $nroreq";
+			$r++;
+				
+			$sqlAcuerdos = "SELECT nroacuerdo FROM aculiquiusimra where nrorequerimiento = $nroreq";
+			$resAcuerdos = mysql_query($sqlAcuerdos,$db);
+			while ($rowAcuerdos = mysql_fetch_array($resAcuerdos)) {
+				$nroacuActivar = $rowAcuerdos['nroacuerdo'];
+				$sqlInsercionesUsimra[$r] = "UPDATE cabacuerdosusimra SET estadoacuerdo = 1 WHERE cuit = $cuit and nroacuerdo = $nroacuActivar";
+				$r++;
+			}
+
+			if (strlen($campos[10]) > 0) {
+				$acuerdos = explode("-",$campos[10]);
+				for ($i=0; $i<sizeof($acuerdos); $i++) {
+					$nroacu = (int)$acuerdos[$i];
+					$sqlInsercionesUsimra[$r] = "INSERT into aculiquiusimra VALUE($nroreq, $nroacu)";
+					$r++;
+					$sqlInsercionesUsimra[$r] = "UPDATE cabacuerdosusimra SET estadoacuerdo = 5 WHERE cuit = $cuit and nroacuerdo = $nroacu";
+					$r++;
+				}
+			}
+			$fechaliq = fechaParaGuardar($campos[1]);
+			$horaliq = $campos[2];
+			$fecinsp = fechaParaGuardar($campos[3]);
+			$totdep = (float)$campos[4];
+			$totint = (float)$campos[5];
+			$gastos = (float)$campos[6];
+			$totliq = (float)$campos[7];
+			$nroreso = (int)$campos[8];
+			$nrosert = (int)$campos[9];
+			$operador = $campos[11];
+			$liqCargadasUsimra[$n] = array('reque' => $nroreq);
+			$sqlInsercionesUsimra[$r] = "UPDATE cabliquiusimra SET fechaliquidacion = '$fechaliq', horaliquidacion = '$horaliq', fechainspeccion = '$fecinsp', deudanominal = $totdep, intereses =  $totint, gtosadmin = $gastos, totalliquidado = $totliq, nroresolucioninspeccion = $nroreso, nrocertificadodeuda = $nrosert, operadorliquidador = '$operador' WHERE nrorequerimiento = $nroreq";
+			$r++;
+			$n++;
+		}
+	}
+	fclose($file);
+}
+
+//INSERTAMOS A LA BASE LOS CIERRES
+try {
+	$hostname = $_SESSION['host'];
+	$dbname = $_SESSION['dbname'];
+	$dbh = new PDO("mysql:host=$hostname;dbname=$dbname",$_SESSION['usuario'],$_SESSION['clave']);
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$dbh->beginTransaction();
+	
+	//OSPIM
+	foreach($sqlInserciones as $sqlospim) {
+		$dbh->exec($sqlospim);
+	}
+	
+	//USIMRA
+	foreach($sqlInsercionesUsimra as $sqlusimra) {
+		$dbh->exec($sqlusimra);
+	}
+	
+	$dbh->commit();
+	if (file_exists($direArc)) unlink($direArc);
+	if (file_exists($direArcUsimra)) unlink($direArcUsimra);
+}catch (PDOException $e) {
+	echo $e->getMessage();
+	$dbh->rollback();
+}
 ?>
 
 
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>.: Cierre de Liquidacion :.</title>
-
 <style>
 A:link {text-decoration: none;color:#0033FF}
 A:visited {text-decoration: none}
@@ -123,23 +174,17 @@ A:hover {text-decoration: none;color:#00FFFF }
 	font-size: 18px;
 }
 </style>
-<script language="JavaScript">
-function borrarArchivo(dire){
-	a= window.open(dire,"borrarArchivo",
-	"toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=800, height=500, top=10, left=10");
-}
-</script>
 </head>
 
 <body bgcolor="#CCCCCC">
 <div align="center">
-  <p class="Estilo2"><span style="text-align:center">
-    <input type="reset" name="volver" value="Volver" onclick="location.href = 'menuFiscalizacion.php'"/>
-  </span></p>
+  <p><input type="button" name="volver" value="Volver" onclick="location.href = 'menuFiscalizacion.php'"/></p>
   <p class="Estilo2">Resultado del proceso de cierra de liquidación </p>
-  <?php if ($liqCargadas != 0) { ?>
+  
+  <p class="Estilo2">O.S.P.I.M.</p>
+  <?php if (sizeof($liqCargadas) != 0) { ?>
   		 <p class="Estilo2">El archivo Seguimiento.txt conten&iacute;a <?php echo sizeof($liqCargadas) ?> líneas</p>
-			  <table width="900" border="1" align="center">
+			  <table style="width: 900; text-align: center;" border="1" >
 					<tr>
 					  <th>Req Nro.</th>
 					  <th>C.U.I.T.</th>
@@ -154,51 +199,79 @@ function borrarArchivo(dire){
 			  			$sqlControlLiqui = "SELECT *, r.cuit as cuit FROM cabliquiospim c, reqfiscalizospim r where c.nrorequerimiento = $nroreq and c.nrorequerimiento = r.nrorequerimiento";
 						$resControlLiqui = mysql_query($sqlControlLiqui,$db);
 						$rowControlLiqui = mysql_fetch_array($resControlLiqui);
-			  			
 						//consultamos las liquidacioens y los aceurdos abscorvidos
-						print("<tr align='center'>");
-						print("<td>".$rowControlLiqui['nrorequerimiento']."</td>");
-						print("<td>".$rowControlLiqui['cuit']."</td>");
-						print("<td>".$rowControlLiqui['fechaliquidacion']." / ".$rowControlLiqui['horaliquidacion']."</td>");
-						print("<td>".$rowControlLiqui['totalliquidado']."</td>");
-						
 						$acuAbsor = "";
 						$sqlAcuLiqui = "SELECT * FROM aculiquiospim WHERE nrorequerimiento = $nroreq";
 						$resAcuLiqui = mysql_query($sqlAcuLiqui,$db);
 						while ($rowAcuLiqui = mysql_fetch_array($resAcuLiqui)) {
 							$acuAbsor = $acuAbsor." * ".$rowAcuLiqui['nroacuerdo'];
 						}
-						print("<td>".$acuAbsor."</td>");
-						
-						print("<td>".$rowControlLiqui['operadorliquidador']."</td>");
 						$liqAnulada = "";
 						if ($rowControlLiqui['liquidacionanulada'] == 1) {
 							$liqAnulada = "LIQ. ANULADA <br>(".$rowControlLiqui['usuarioanulacion']." - ".$rowControlLiqui['fechaanulacion'].")";
-						} 
-						print("<td>".$liqAnulada."</td>");
-						print("</tr>");
-					}
-			  ?>
+						} ?>
+						<tr align='center'>
+							<td><?php echo $rowControlLiqui['nrorequerimiento'] ?></td>
+							<td><?php echo $rowControlLiqui['cuit'] ?></td>
+							<td><?php echo $rowControlLiqui['fechaliquidacion']." / ".$rowControlLiqui['horaliquidacion'] ?></td>
+							<td><?php echo $rowControlLiqui['totalliquidado'] ?></td>
+							<td><?php echo $acuAbsor ?></td>
+							<td><?php echo $rowControlLiqui['operadorliquidador'] ?></td>
+							<td><?php echo $liqAnulada ?></td>
+						</tr>
+	   <?php	 } ?>
 			</table>
-			  <p>
-			    <?php } else {
-				print("<p><div align='center' style='color:#000000'><b> NO SE ENCONTRÓ EL ARCHIVO Seguimiento.txt </b></div></p>");
-		  		} 
-	
-		  if ($liqUsimra == 0) { 
-		  		print("<p><div align='center' style='color:#000000'><b> NO SE ENCONTRÓ EL ARCHIVO SegUSIMRA.txt </b></div></p>");
-		  }	
-		  if ($liqUsimra == 1) {
-				print("<p><div align='center' style='color:#0033FF'><b> SE SUBIO EL ARCHIVO SegUSIMRA.txt </b></div></p>"); ?>
-				<input type="button" name="borrar" value="Borrar Archivo U.S.I.M.R.A." onclick="borrarArchivo('borrarArchivo.php')" /> 
-    <?php } 
-		  if ($liqUsimra == 2) {
-				print("<p><div align='center' style='color:#FF0000'><b> SE PRODUJO UN ERROR INTENTANDO SUBIR EL ARCHIVO SegUSIMRA.txt </b></div></p>");
-		  }
-		  
-	?>
-  </p>
-			  <p><input type="button" name="imprimir" value="Imprimir" onclick="window.print();"/></p>
+<?php 	  } else { ?>
+			 <p><b style="color:blue;"> NO SE ENCONTRÓ EL ARCHIVO Seguimiento.txt </b></p>
+<?php	  }  ?>
+		
+	<p class="Estilo2">U.S.I.M.R.A.</p>
+  	<?php if (sizeof($liqCargadasUsimra) != 0) { ?>
+  		 <p class="Estilo2">El archivo SegUsimra.txt conten&iacute;a <?php echo sizeof($liqCargadasUsimra) ?> líneas</p>
+			  <table style="width: 900; text-align: center;" border="1" >
+					<tr>
+					  <th>Req Nro.</th>
+					  <th>C.U.I.T.</th>
+					  <th>Fecha</th>
+					  <th>Total Liquidado</th>
+					  <th>Acuerdos Absorvidos</th>
+					  <th>Operador</th>
+					  <th>Control</th>
+					</tr>
+			  <?php for ($i=0; $i < sizeof($liqCargadasUsimra); $i++) {
+			  			$nroreq = $liqCargadasUsimra[$i]['reque'];
+			  			$sqlControlLiqui = "SELECT *, r.cuit as cuit FROM cabliquiusimra c, reqfiscalizusimra r where c.nrorequerimiento = $nroreq and c.nrorequerimiento = r.nrorequerimiento";
+						$resControlLiqui = mysql_query($sqlControlLiqui,$db);
+						$rowControlLiqui = mysql_fetch_array($resControlLiqui);
+			  			
+						//consultamos las liquidacioens y los aceurdos abscorvidos
+						$acuAbsor = "";
+						$sqlAcuLiqui = "SELECT * FROM aculiquiusimra WHERE nrorequerimiento = $nroreq";
+						$resAcuLiqui = mysql_query($sqlAcuLiqui,$db);
+						while ($rowAcuLiqui = mysql_fetch_array($resAcuLiqui)) {
+							$acuAbsor = $acuAbsor." * ".$rowAcuLiqui['nroacuerdo'];
+						}
+						$liqAnulada = "";
+						if ($rowControlLiqui['liquidacionanulada'] == 1) {
+							$liqAnulada = "LIQ. ANULADA <br>(".$rowControlLiqui['usuarioanulacion']." - ".$rowControlLiqui['fechaanulacion'].")";
+						} ?>
+						
+						<tr align='center'>
+							<td><?php echo $rowControlLiqui['nrorequerimiento'] ?></td>
+							<td><?php echo $rowControlLiqui['cuit'] ?></td>
+							<td><?php echo $rowControlLiqui['fechaliquidacion']." / ".$rowControlLiqui['horaliquidacion'] ?></td>
+							<td><?php echo $rowControlLiqui['totalliquidado'] ?></td>
+							<td><?php echo $acuAbsor ?></td>
+							<td><?php echo $rowControlLiqui['operadorliquidador'] ?></td>
+							<td><?php echo $liqAnulada ?></td>
+						</tr>
+		 <?php		} ?>
+			</table>
+<?php 	  } else { ?>
+			 <p><b style="color:blue;"> NO SE ENCONTRÓ EL ARCHIVO SegUSIMRA.txt </b></p>
+<?php	  }  ?>
+		
+		  <p><input type="button" name="imprimir" value="Imprimir" onclick="window.print();"/></p>
 </div>
 </body>
 </html>
