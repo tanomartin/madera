36cd50ef1eb460bd668e09e304b51f09e793944f
Juan Carlos Bolognese <jcbolognese@gmail.com>
2015-10-16 16:55:23 -0300

    Correccion del path hacia la libreria

--- ospim/auditoria/autorizaciones/atiendeAutorizacion.php
+++ ospim/auditoria/autorizaciones/atiendeAutorizacion.php
@@ -2,7 +2,6 @@
 include($libPath."controlSessionOspim.php");
 include($libPath."fechas.php");
 $nrosolicitud=$_GET['nroSolicitud'];
-
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
