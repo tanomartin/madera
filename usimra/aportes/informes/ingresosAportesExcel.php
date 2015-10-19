9eeb7eb9d14969af1da67fcbaf8c5cb080e0593b
Juan Carlos Bolognese <jcbolognese@gmail.com>
2015-10-19 20:43:22 -0300

    Se ajusta el dato para la columna de porcentaje de afectacion de ingreso

--- usimra/aportes/informes/ingresosAportesExcel.php
+++ usimra/aportes/informes/ingresosAportesExcel.php
@@ -174,7 +174,7 @@ ORDER BY j.codidelega, s.cuit, s.anopago, s.mespago, s.fechapago";
 			$objPHPExcel->getActiveSheet()->setCellValue('I'.$fila, $aportes[aporte100]);
 			$objPHPExcel->getActiveSheet()->setCellValue('J'.$fila, $aportes[aporte150]);
 			$objPHPExcel->getActiveSheet()->setCellValue('K'.$fila, $aportes[montorecargo]);
-			$objPHPExcel->getActiveSheet()->setCellValue('L'.$fila, $aportes[disgdinero]);
+			$objPHPExcel->getActiveSheet()->setCellValue('L'.$fila, ($aportes[disgdinero]/100.00));
 			$objPHPExcel->getActiveSheet()->setCellValue('M'.$fila, $aportes[totaldeposito]);
 			$objPHPExcel->getActiveSheet()->setCellValue('N'.$fila, $aportes[sistemacancelacion]);
 			$objPHPExcel->getActiveSheet()->setCellValue('O'.$fila, $aportes[codigobarra]);
