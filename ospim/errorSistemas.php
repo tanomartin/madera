a2dc08a3a1dd0b9654b484709af0e296bbecb60c
tanomartin <tanomartin05@gmail.com>
2016-07-13 12:58:57 -0300

    agregado de libreria nueva cambio pantallas de error del sistema

--- ospim/errorSistemas.php
+++ ospim/errorSistemas.php
@@ -23,16 +23,16 @@ A:hover {text-decoration:underline; color:FCF63C}
 <body bgcolor="#CCCCCC" link="#D5913A" vlink="#CF8B34" alink="#D18C35">
   <div align="center">
     <p class="Estilo1">&iexcl;&iexcl;ERROR de Sistema!!</p>
-    <p class="Estilo2">Por favor imprimir esta pantalla y llevarla al Dpto. de Sistemas</p>
+    <p class="Estilo2">Cualquier duda imprimir y comunicarse son Dpto. de Sistemas</p>
     <p><img src="img/stop.png" width="128" height="128"> </p>
-    <table border="1" style="width: 200">
+    <table border="1" style="width: 800px">
       <tr>
         <td><strong>P&aacute;gina: </strong></td>
-        <td><?php echo $_GET['error']; ?></td>
+        <td><?php echo $_GET['page']; ?></td>
       </tr>
       <tr>
         <td><strong>ERROR:</strong></td>
-        <td><?php echo $_GET['page']; ?></td>
+        <td><?php echo $_GET['error']; ?></td>
       </tr>
     </table>
     <p><input type="button" name="imprimir" value="Imprimir" onClick="window.print();" /></p>
