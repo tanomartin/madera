81684e833eebb1174f95437aefa32bac922b8980
tanomartin <tanomartin05@gmail.com>
2016-11-01 12:52:59 -0300

    agregado de Block UI

--- ospim/afiliados/procesos/moduloProcesos.php
+++ ospim/afiliados/procesos/moduloProcesos.php
@@ -12,6 +12,16 @@ A:link {text-decoration: none;color:#0033FF}
 A:visited {text-decoration: none}
 A:hover {text-decoration: none;color:#00FFFF }
 </style>
+
+<script src="/madera/lib/jquery.js"></script>
+<script src="/madera/lib/jquery-ui.min.js"></script>
+<script src="/madera/lib/jquery.blockUI.js" type="text/javascript"></script>
+<script>
+function mostrar(dire) {
+	$.blockUI({ message: "<h1>Realizando Proceso Pedido... <br>Esto puede tardar unos minutos.<br> Aguarde por favor</h1>" });
+	window.location = dire;
+}
+</script>
 </head>
 
 <body bgcolor="#CCCCCC">
@@ -25,15 +35,15 @@ A:hover {text-decoration: none;color:#00FFFF }
   <table width="400" border="3">
     <tr>
        <td width="200"><p align="center">Filtro Titulares</p>
-        <p align="center"><a class="enlace" href="filtroTitulares/filtroTitulares.php"><img src="../img/filtro.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+        <p align="center"><a class="enlace" href="javascript:mostrar('filtroTitulares/filtroTitulares.php')"><img src="../img/filtro.png" width="90" height="90" border="0" alt="enviar"/></a></p>
         <p>&nbsp;</p></td>
       <td width="200"><p align="center">Filtro Familiares</p>
-          <p align="center"><a class="enlace" href="filtroFamiliares/filtroFamiliares.php"><img src="../img/filtro.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+          <p align="center"><a class="enlace" href="javascript:mostrar('filtroFamiliares/filtroFamiliares.php')"><img src="../img/filtro.png" width="90" height="90" border="0" alt="enviar"/></a></p>
         <p>&nbsp;</p></td>
     </tr>
     <tr>
        <td width="200"><p align="center">Reactivación Masiva</p>
-          <p align="center"><a class="enlace" href="reactivaTitulares/reactivaTitulares.php"><img src="../img/reactivacion.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+          <p align="center"><a class="enlace" href="javascript:mostrar('reactivaTitulares/reactivaTitulares.php')"><img src="../img/reactivacion.png" width="90" height="90" border="0" alt="enviar"/></a></p>
         <p>&nbsp;</p></td>
        <td width="200"><p align="center">Comparación Padron SSS</p>
           <p align="center"><a class="enlace" href="cruceSSS/menuCruceSSS.php"><img src="../img/mirroring.png" width="90" height="90" border="0" alt="enviar"/></a></p>
