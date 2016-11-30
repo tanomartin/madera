7b7a629338497d2c9ef611e48a1e258936bdce2a
tanomartin <tanomartin05@gmail.com>
2016-11-30 14:17:44 -0300

    arreglo de filtro y reactivacion. Solo verifico fechas sin año

--- ospim/afiliados/procesos/filtroTitulares/filtroTitulares.php
+++ ospim/afiliados/procesos/filtroTitulares/filtroTitulares.php
@@ -28,14 +28,14 @@ $fechaDesempleo = date ( 'Y-m-j', $fechaDesempleo );
 $sqlTitulares = "SELECT DISTINCT cuil FROM titulares t where fechacarnet <= '" . $fechaInicio . "' and tipoafiliado != 'U' and codidelega not in (1000,1001)";
 //echo $sqlTitulares . "<br>";
 
-$sqlDDJJ = "SELECT DISTINCT cuil FROM detddjjospim d where where anoddjj >= ".date("Y",strtotime($fechaInicio))." and STR_TO_DATE(CONCAT('01/', mesddjj, '/', anoddjj ),'%d/%m/%Y') BETWEEN '$fechaInicio' and '$fecha'";
+$sqlDDJJ = "SELECT DISTINCT cuil FROM detddjjospim d where where STR_TO_DATE(CONCAT('01/', mesddjj, '/', anoddjj ),'%d/%m/%Y') BETWEEN '$fechaInicio' and '$fecha'";
 //echo $sqlDDJJ . "<br>";
 
-$sqlPagos = "SELECT DISTINCT cuil FROM afiptransferencias d where anopago >= ".date("Y",strtotime($fechaInicio))." and STR_TO_DATE(CONCAT('01/', mespago, '/', anopago ),'%d/%m/%Y') BETWEEN '$fechaInicio' and '$fecha'";
+$sqlPagos = "SELECT DISTINCT cuil FROM afiptransferencias d where STR_TO_DATE(CONCAT('01/', mespago, '/', anopago ),'%d/%m/%Y') BETWEEN '$fechaInicio' and '$fecha'";
 //echo $sqlPagos . "<br>";
 
 //$sqlDesempleo = "SELECT DISTINCT cuilbeneficiario FROM desempleosss d where anodesempleo = " . date ( "Y", strtotime ( $fechaDesempleo ) ) . " and mesdesempleo = " . date ( "n", strtotime ( $fechaDesempleo ) ) . " and parentesco = 0";
-$sqlDesempleo = "SELECT DISTINCT cuilbeneficiario FROM desempleosss d where anodesempleo >= ".date("Y",strtotime($fechaInicio))." and STR_TO_DATE(CONCAT('01/', mesdesempleo, '/', anodesempleo ),'%d/%m/%Y') BETWEEN '$fechaInicio' and '$fecha'";
+$sqlDesempleo = "SELECT DISTINCT cuilbeneficiario FROM desempleosss d where STR_TO_DATE(CONCAT('01/', mesdesempleo, '/', anodesempleo ),'%d/%m/%Y') BETWEEN '$fechaInicio' and '$fecha'";
 //echo $sqlDesempleo . "<br><br>";
 
 $resTitulares = mysql_query ( $sqlTitulares, $db );
