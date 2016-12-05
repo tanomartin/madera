acbefb4ec18fcd5de17f324b4d6192b8f4d933a8
tanomartin <tanomartin05@gmail.com>
2016-12-05 14:36:39 -0300

    arreglo de vista

--- ospim/acuerdos/valores/valoresRealizados.php
+++ ospim/acuerdos/valores/valoresRealizados.php
@@ -30,7 +30,7 @@ A:hover {text-decoration: none;color:#00FFFF }
 			theme: 'blue', 
 			widthFixed: true, 
 			widgets: ["zebra", "filter"], 
-			headers:{2:{sorter:false, filter:false}, 3:{sorter:false, filter:false}},
+			headers:{3:{sorter:false, filter:false}, 4:{sorter:false, filter:false}},
 			widgetOptions : { 
 				filter_cssFilter   : '',
 				filter_childRows   : false,
