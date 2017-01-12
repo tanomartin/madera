be2b4d14a0ec0e0fe3ec47650b4a43a72a903348
tanomartin <tanomartin05@gmail.com>
2017-01-12 11:46:07 -0300

    arreglo error cabecera empresa

--- comun/empresas/abm/disgregaNuevaJurisdiccion.php
+++ comun/empresas/abm/disgregaNuevaJurisdiccion.php
@@ -7,13 +7,7 @@ $sql = "select * from empresas where cuit = $cuit";
 $result = mysql_query($sql,$db); 
 $row = mysql_fetch_array($result); 
 
-$sqllocalidad = "select * from localidades where codlocali = $row[codlocali]";
-$resultlocalidad = mysql_query($sqllocalidad,$db); 
-$rowlocalidad = mysql_fetch_array($resultlocalidad); 
-
-$sqlprovi =  "select * from provincia where codprovin = $row[codprovin]";
-$resultprovi = mysql_query($sqlprovi,$db); 
-$rowprovi = mysql_fetch_array($resultprovi);
+include($libPath."cabeceraEmpresaConsulta.php");
 
 $sqljuris = "select * from jurisdiccion where cuit = $cuit";
 $resjuris = mysql_query($sqljuris,$db); 
