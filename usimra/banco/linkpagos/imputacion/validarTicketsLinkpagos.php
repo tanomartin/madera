19ba8def7394b2140d53cc6b0ae7bc067da4f700
Juan Carlos Bolognese <jcbolognese@gmail.com>
2017-05-18 13:44:22 -0300

    Ajuste del control de la existencia del cuit en la base de datos de empresas

--- usimra/banco/linkpagos/imputacion/validarTicketsLinkpagos.php
+++ usimra/banco/linkpagos/imputacion/validarTicketsLinkpagos.php
@@ -113,51 +113,58 @@ try {
 							<td><?php echo $cuitbanco; ?></td>
 							<td><?php echo $importebanco; ?></td>
 <?php
-					$sqlControlTicket="SELECT COUNT(*) FROM vinculadocuusimra WHERE nrcuit = '$cuitbanco' AND referencia = '$referenciabanco'";
-					$resultControlTicket=$dbh->query($sqlControlTicket);
-					if($resultControlTicket->fetchColumn()!=0) {
-						$sqlBuscaDDJJ="SELECT COUNT(d.nrctrl) AS cantdj, SUM((d.totapo+d.recarg)) AS totdep FROM ddjjusimra d, vinculadocuusimra v WHERE d.nrcuil = '$cuil' AND d.nrcuit = '$cuitbanco' AND d.nrcuit = v.nrcuit AND d.nrctrl = v.nrctrl AND v.referencia = '$referenciabanco' GROUP BY v.nrcuit, v.referencia";
-						$resultBuscaDDJJ=$dbh->query($sqlBuscaDDJJ);
-						foreach($resultBuscaDDJJ as $totaddjj) {
-							$cantddjj = $totaddjj[cantdj];
-							$impoddjj = $totaddjj[totdep];
-							if($cantddjj>0) {
-								if($impoddjj==$importebanco) {
-									$sqlActualizaLink="UPDATE linkaportesusimra SET fechavalidacion = '$fechavalidacion', usuariovalidacion = '$usuariovalidacion' WHERE fechaarchivo = '$fechabanco' AND idmovimiento = $movimientobanco";
-									if($resultActualizaLink = $dbh->query($sqlActualizaLink)) {
-										$cantvali++;
-										$listastatus="Ticket Validado";
-										$listamensaje="TODOS LOS DATOS DE LA IMPUTACION DE LINK PAGOS SON CORRECTOS.";
-									}
-								} else {
-									$cantnova++;
-									$listastatus="Ticket No Validado";
-									$listamensaje="EL IMPORTE (".$importebanco.") ACREDITADO POR LINK PAGOS ES DISTINTO AL DEL TICKET GENERADO (".$impoddjj.").";
-								}
-							} else {
-								$sqlBuscaValidada="SELECT COUNT(d.nrocontrol) AS cantdj, SUM((d.totalaporte+d.recargo)) AS totdep FROM cabddjjusimra d, vinculadocuusimra v WHERE d.cuil = '$cuil' AND d.cuit = '$cuitbanco' AND d.cuit = v.nrcuit AND d.nrocontrol = v.nrctrl AND v.referencia = '$referenciabanco' GROUP BY v.nrcuit, v.referencia";
-								$resultBuscaValidada=$dbh->query($sqlBuscaValidada);
-								if($resultBuscaValidada->fetchColumn(0)>0) {
-									if($resultBuscaValidada->fetchColumn(1)==$importebanco) {
-										$sqlBancoValidada="SELECT * FROM linkaportesusimra WHERE cuit = '$cuitbanco' AND referencia = '$referenciabanco' AND importe = $importebanco AND fechavalidacion != '0000-00-00 00:00:00' ORDER BY fechaarchivo DESC, fechadeposito DESC, idmovimiento DESC LIMIT 1";
-										$resultBancoValidada=$dbh->query($sqlBancoValidada);
-										foreach($resultBancoValidada as $bancovalidada) {
-											$cantnova++;
-											$listastatus="Ticket No Validado";
-											$listamensaje="VALIDACION ANTERIOR PARA TICKET PRESENTADO EL ".invertirFecha($bancovalidada[fechaarchivo])." DEPOSITADO EL ".invertirFecha($bancovalidada[fechadeposito]).".";
+					$sqlControlBuscaEmpresa="SELECT COUNT(*) FROM empresas WHERE cuit = '$cuitbanco'";
+					$resultControlBuscaEmpresa=$dbh->query($sqlControlBuscaEmpresa);
+					if($resultControlBuscaEmpresa->fetchColumn()!=0) {
+						$sqlControlTicket="SELECT COUNT(*) FROM vinculadocuusimra WHERE nrcuit = '$cuitbanco' AND referencia = '$referenciabanco'";
+						$resultControlTicket=$dbh->query($sqlControlTicket);
+						if($resultControlTicket->fetchColumn()!=0) {
+							$sqlBuscaDDJJ="SELECT COUNT(d.nrctrl) AS cantdj, SUM((d.totapo+d.recarg)) AS totdep FROM ddjjusimra d, vinculadocuusimra v WHERE d.nrcuil = '$cuil' AND d.nrcuit = '$cuitbanco' AND d.nrcuit = v.nrcuit AND d.nrctrl = v.nrctrl AND v.referencia = '$referenciabanco' GROUP BY v.nrcuit, v.referencia";
+							$resultBuscaDDJJ=$dbh->query($sqlBuscaDDJJ);
+							foreach($resultBuscaDDJJ as $totaddjj) {
+								$cantddjj = $totaddjj[cantdj];
+								$impoddjj = $totaddjj[totdep];
+								if($cantddjj>0) {
+									if($impoddjj==$importebanco) {
+										$sqlActualizaLink="UPDATE linkaportesusimra SET fechavalidacion = '$fechavalidacion', usuariovalidacion = '$usuariovalidacion' WHERE fechaarchivo = '$fechabanco' AND idmovimiento = $movimientobanco";
+										if($resultActualizaLink = $dbh->query($sqlActualizaLink)) {
+											$cantvali++;
+											$listastatus="Ticket Validado";
+											$listamensaje="TODOS LOS DATOS DE LA IMPUTACION DE LINK PAGOS SON CORRECTOS.";
 										}
+									} else {
+										$cantnova++;
+										$listastatus="Ticket No Validado";
+										$listamensaje="EL IMPORTE (".$importebanco.") ACREDITADO POR LINK PAGOS ES DISTINTO AL DEL TICKET GENERADO (".$impoddjj.").";
 									}
 								} else {
-									$cantnova++;
-									$listastatus="Ticket No Validado";
-									$listamensaje="TICKET RELACIONADO AL PAGO INEXISTENTE.";
+									$sqlBuscaValidada="SELECT COUNT(d.nrocontrol) AS cantdj, SUM((d.totalaporte+d.recargo)) AS totdep FROM cabddjjusimra d, vinculadocuusimra v WHERE d.cuil = '$cuil' AND d.cuit = '$cuitbanco' AND d.cuit = v.nrcuit AND d.nrocontrol = v.nrctrl AND v.referencia = '$referenciabanco' GROUP BY v.nrcuit, v.referencia";
+									$resultBuscaValidada=$dbh->query($sqlBuscaValidada);
+									if($resultBuscaValidada->fetchColumn(0)>0) {
+										if($resultBuscaValidada->fetchColumn(1)==$importebanco) {
+											$sqlBancoValidada="SELECT * FROM linkaportesusimra WHERE cuit = '$cuitbanco' AND referencia = '$referenciabanco' AND importe = $importebanco AND fechavalidacion != '0000-00-00 00:00:00' ORDER BY fechaarchivo DESC, fechadeposito DESC, idmovimiento DESC LIMIT 1";
+											$resultBancoValidada=$dbh->query($sqlBancoValidada);
+											foreach($resultBancoValidada as $bancovalidada) {
+												$cantnova++;
+												$listastatus="Ticket No Validado";
+												$listamensaje="VALIDACION ANTERIOR PARA TICKET PRESENTADO EL ".invertirFecha($bancovalidada[fechaarchivo])." DEPOSITADO EL ".invertirFecha($bancovalidada[fechadeposito]).".";
+											}
+										}
+									} else {
+										$cantnova++;
+										$listastatus="Ticket No Validado";
+										$listamensaje="TICKET RELACIONADO AL PAGO INEXISTENTE.";
+									}
 								}
 							}
+						} else {
+							$cantnova++;
+							$listastatus="Ticket No Validado";
+							$listamensaje="TICKET RELACIONADO AL PAGO INEXISTENTE.";
 						}
 					} else {
-						$cantnova++;
-						$listastatus="Ticket No Validado";
-						$listamensaje="TICKET RELACIONADO AL PAGO INEXISTENTE.";
+						$listastatus="Pago No Imputado";
+						$listamensaje="EMPRESA INEXISTENTE EN LA BASE DE DATOS DE USIMRA.";
 					}
 ?>
 							<td><?php echo $listastatus; ?></td>
