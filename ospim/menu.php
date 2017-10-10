6565e6caa384fbcbb474b9b5a1672ffe2770da7b
tanomartin <tanomartin05@gmail.com>
2017-10-10 14:57:38 -0300

    arreglo menu principal

--- ospim/menu.php
+++ ospim/menu.php
@@ -26,78 +26,54 @@ function abrirModulo(dire, titulo) {
 <div align="center">
   <p><span class="Estilo1">Men&uacute; Principal - Sistema OSPIM</span></p>
   <p><img src="img/logo.png" width="168" height="142" /></p>
-  <table width="600" border="1">
+  <table width="600" border="1" style="text-align: center;vertical-align: middle;">
     <tr>
-      <td> 
-	  <div align="center">
+      <td width="200"> 
         <p>Acuerdos</p>
         <p><a href="javascript:abrirModulo('acuerdos/menuAcuerdos.php','AcuerdosOspim')"><img src="img/acuerdos.png" width="90" height="90" border="0"/></a></p>
-        <p>&nbsp;</p>
-        </div>
 	  </td>
-      <td>
-	  <div align="center">
+      <td width="200">
 	    <p>Empresas</p>
 	    <p><a href="javascript:abrirModulo('../comun/empresas/menuEmpresa.php?origen=ospim','EmpresasOspim')"><img src="img/empresa.png" width="90" height="90" border="0" alt="enviar"/></a></p>
-	    <p>&nbsp;</p>
-      </div>
 	  </td>
-      <td>
-	  <div align="center">
+      <td width="200">
 	    <p>Afiliados</p>
 	    <p><a href="javascript:abrirModulo('afiliados/menuAfiliados.php','AfiliadosOspim')"><img src="img/afiliados.png" width="90" height="90" border="0" /></a></p>
-	    <p>&nbsp;</p>
-      </div>
 	  </td>
     </tr>
 	
 	<tr>
       <td>
-	  <div align="center">
 	    <p>Fiscalizaci&oacute;n</p>
 	    <p><a href="javascript:abrirModulo('fiscalizacion/menuFiscalizacion.php','FiscalizacionOspim')"><img src="img/fiscalizacion.png" width="90" height="90" border="0" alt="enviar"/></a></p>
-	    <p>&nbsp;</p>
-      </div>
 	  </td>	
-      <td width="196"><div align="center">
+      <td>
 	    <p>Auditoria Medica </p>
 	    <p><a href="javascript:abrirModulo('auditoria/menuAuditoria.php','AuditoriaOspim')"><img src="img/auditoria.png" width="90" height="90" border="0" alt="enviar"/></a></p>
-	    <p>&nbsp;</p>
-      </div></td> 	
+	  </td> 	
 	  <td>
-	  <div align="center">
 	    <p>Legales </p>
 	    <p><a href="javascript:abrirModulo('legales/menuLegales.php','LegalesOspim')"><img src="img/juicios.png" width="90" height="90" border="0" alt="enviar"/></a></p>
-	    <p>&nbsp;</p>
-      </div>
 	  </td>	  
     </tr>
+    
 	<tr>
 	  <td> <?php if ($_SESSION['usuario'] == 'sistemas') { ?> 
-	  <div align="center">
 			<p>Sistemas</p>
 			<p><a href="javascript:abrirModulo('sistemas/menuSistemas.php','SistemasOspim')"><img src="img/sistemas.png" width="90" height="90" border="0" alt="enviar"/></a></p>
-			<p>&nbsp;</p>
-	  </div>
-	  <?php } ?></td>
+	  <?php } ?>
+	  </td>
 	  <td>
-	 <div align="center">
 	    <p>Tesorería </p>
 	    <p><a href="javascript:abrirModulo('tesoreria/menuTesoreria.php','TesoreriaOspim')"><img src="img/tesoreria.png" width="90" height="90" border="0" alt="enviar"/></a></p>
-	    <p>&nbsp;</p>
-      </div>
 	  </td>
-	  <td> <div align="center">
+	  <td>
 	    <p>Contaduría </p>
 	    <p><a href="javascript:abrirModulo('contaduria/menuContaduria.php','ContaduriaOspim')"><img src="img/contaduria.png" width="90" height="90" border="0" alt="enviar"/></a></p>
-	    <p>&nbsp;</p>
-      </div></td>
+	  </td>
     </tr>
   </table>
-   <p><strong><a href="logout.php">
-     <input type="button" name="salir" value="SALIR" onclick="location.href='logout.php'" />
-  </a></strong></p>
+   <p><a href="logout.php"><input type="button" name="salir" value="SALIR" onclick="location.href='logout.php'" /></a></p>
 </div>
 </body>
-
 </html>
