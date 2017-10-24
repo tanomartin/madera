80d59d75edb0b6f35ccc45c777c3f70e1852c00e
tanomartin <tanomartin05@gmail.com>
2017-10-24 11:39:29 -0300

    Subida busqueda de midicamentos

--- ospim/auditoria/menuAuditoria.php
+++ ospim/auditoria/menuAuditoria.php
@@ -12,34 +12,41 @@ include($libPath."controlSessionOspim.php"); ?>
 <body bgcolor="#CCCCCC">
 <div align="center">
   <h2>Men&uacute; Auditoria Medica </h2>
-  <table width="600" border="3">
+  <table width="600" border="1" style="text-align: center;vertical-align: middle;">
     <tr>
-      <td width="200"><p align="center">Prestadores</p>
-          <p align="center"><a class="enlace" href="../moduloNoDisponible.php"><img src="img/prestador.png" width="90" height="90" border="0" alt="enviar"/></a></p>
-        <p align="center">&nbsp;</p></td>
-      <td width="200"><p align="center">Autorizaciones</p>
-          <p align="center"><a class="enlace" href="autorizaciones/moduloAutorizaciones.php"><img src="img/auditoria.png" width="90" height="90" border="0" alt="enviar"/></a></p>
-        <p align="center">&nbsp;</p></td>
-		<td width="200"><p align="center">Nomencladores de Practicas</p>
-          <p align="center"><a class="enlace" href="../moduloNoDisponible.php"><img src="img/nomenclador.png" width="90" height="90" border="0" alt="enviar"/></a></p>
-        <p align="center">&nbsp;</p></td>
+      <td width="200">
+      	  <p>Prestadores</p>
+          <p><a class="enlace" href="../moduloNoDisponible.php"><img src="img/prestador.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+      </td> 	
+      <td width="200">
+      	  <p>Autorizaciones</p>
+          <p><a class="enlace" href="autorizaciones/moduloAutorizaciones.php"><img src="img/auditoria.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+	  </td>
+	  <td width="200">
+	  	  <p>Nomencladores de Practicas</p>
+          <p><a class="enlace" href="../moduloNoDisponible.php"><img src="img/nomenclador.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+      </td>  
     </tr>
     <tr>
-      <td><p align="center">S.U.R. </p>
-        <p align="center"><a class="enlace" href="sur/menuSur.php"><img src="img/sur.jpg" width="90" height="90" border="0" alt="enviar"/></a></p>
-        <p align="center">&nbsp;</p></td>
-      <td><p align="center">Programa de Prevenci&oacute;n </p>
-      <p align="center"><a class="enlace" href="../moduloNoDisponible.php"><img src="img/prevencion.png" width="90" height="90" border="0" alt="enviar"/></a></p>
-      <p>&nbsp;</p></td>
-      <td><p align="center">Medicamentos (Alfa Beta)</p>
-      <p align="center"><a class="enlace" href="../moduloNoDisponible.php"><img src="img/medicamentos.png" width="90" height="90" border="0" alt="enviar"/></a></p>
-      <p>&nbsp;</p></td>
+      <td>
+        <p>S.U.R. </p>
+        <p><a class="enlace" href="sur/menuSur.php"><img src="img/sur.jpg" width="90" height="90" border="0" alt="enviar"/></a></p>
+      </td>
+      <td>
+      	<p>Programa de Prevenci&oacute;n </p>
+      	<p><a class="enlace" href="../moduloNoDisponible.php"><img src="img/prevencion.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+      </td>
+      <td>
+      	<p>Medicamentos (Alfa Beta)</p>
+      	<p><a class="enlace" href="medicamentos/menuMedicamentos.php"><img src="img/medicamentos.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+      </td>
     </tr>
     <?php if ($_SESSION['usuario'] == 'sistemas' || $_SESSION['usuario'] == 'sgiraudo' || $_SESSION['usuario'] == 'gflongo') { ?> 
 	    <tr>
-	    	<td><p align="center">Gestión y Seguimiento </p>
-      			<p align="center"><a class="enlace" href="../moduloNoDisponible.php"><img src="img/seguimiento.png" width="90" height="90" border="0" alt="enviar"/></a></p>
-      			<p>&nbsp;</p></td>
+	    	<td>
+	    		<p>Gestión y Seguimiento </p>
+      			<p><a class="enlace" href="seguimiento/menuSeguimiento.php"><img src="img/seguimiento.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+      		</td>
 	    	<td></td>
 	    	<td></td>
 	    </tr>
