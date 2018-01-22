dca51971611c7f031e956e9fb93ba7a8d2aae0a7
tanomartin <tanomartin05@gmail.com>
2018-01-22 10:38:05 -0300

    subida de menu odontologico

--- ospim/auditoria/menuAuditoria.php
+++ ospim/auditoria/menuAuditoria.php
@@ -15,42 +15,45 @@ include($libPath."controlSessionOspim.php"); ?>
   <table width="600" border="1" style="text-align: center;vertical-align: middle;">
     <tr>
       <td width="200">
-      	  <p>Prestadores</p>
-          <p><a class="enlace" href="prestadores/menuPrestadores.php"><img src="img/prestador.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+      	  <p>PRESTADORES</p>
+          <p><a href="prestadores/menuPrestadores.php"><img src="img/prestador.png" width="90" height="90" border="0" alt="enviar"/></a></p>
       </td> 	
       <td width="200">
-      	  <p>Autorizaciones</p>
-          <p><a class="enlace" href="autorizaciones/moduloAutorizaciones.php"><img src="img/auditoria.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+      	  <p>AUTORIZACIONES</p>
+          <p><a href="autorizaciones/moduloAutorizaciones.php"><img src="img/auditoria.png" width="90" height="90" border="0" alt="enviar"/></a></p>
 	  </td>
 	  <td width="200">
-	  	  <p>Nomencladores de Practicas</p>
-          <p><a class="enlace" href="nomenclador/menuNomenclador.php"><img src="img/nomenclador.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+	  	  <p>NOMENCLADORES</p>
+          <p><a href="nomenclador/menuNomenclador.php"><img src="img/nomenclador.png" width="90" height="90" border="0" alt="enviar"/></a></p>
       </td>  
     </tr>
     <tr>
       <td>
         <p>S.U.R. </p>
-        <p><a class="enlace" href="sur/menuSur.php"><img src="img/sur.jpg" width="90" height="90" border="0" alt="enviar"/></a></p>
+        <p><a href="sur/menuSur.php"><img src="img/sur.jpg" width="90" height="90" border="0" alt="enviar"/></a></p>
       </td>
       <td>
-      	<p>Programa de Prevenci&oacute;n </p>
-      	<p><a class="enlace" href="../moduloNoDisponible.php"><img src="img/prevencion.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+      	<p>PROG. DE PREVENCION </p>
+      	<p><a href="../moduloNoDisponible.php"><img src="img/prevencion.png" width="90" height="90" border="0" alt="enviar"/></a></p>
       </td>
       <td>
-      	<p>Medicamentos (Alfa Beta)</p>
-      	<p><a class="enlace" href="medicamentos/menuMedicamentos.php"><img src="img/medicamentos.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+      	<p>MEDICAMENTOS (ALFABETA)</p>
+      	<p><a href="medicamentos/menuMedicamentos.php"><img src="img/medicamentos.png" width="90" height="90" border="0" alt="enviar"/></a></p>
       </td>
     </tr>
     <tr>
 	  <td>
-	   	<p>Gestión y Seguimiento </p>
-      	<p><a class="enlace" href="seguimiento/menuSeguimiento.php"><img src="img/seguimiento.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+	   	<p>GESTION Y SEGUIMIENTO </p>
+      	<p><a href="seguimiento/menuSeguimiento.php"><img src="img/seguimiento.png" width="90" height="90" border="0" alt="enviar"/></a></p>
       </td>
 	  <td>
-	   	<p>Liquidacion Prestadores</p>
-		<p><a class="enlace" href="../moduloNoDisponible.php"><img src="img/liquidacion.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+	   	<p>LIQUIDACION PRESTADORES</p>
+		<p><a href="../moduloNoDisponible.php"><img src="img/liquidacion.png" width="90" height="90" border="0" alt="enviar"/></a></p>
 	  </td>
-	  <td></td>
+	  <td>
+	  	<p>ODONTOLOGIA</p>
+	  	<p><a href="ondontologia/moduloOdontologico.php"><img src="img/odonto.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+	  </td>
     </tr>
   </table>
 </div>
