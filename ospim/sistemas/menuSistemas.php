a6a440a2b425cfa00701341c31abd5e52c641793
tanomartin <tanomartin05@gmail.com>
2018-03-19 12:01:55 -0300

    arreglo de titulos a mayusculas

--- ospim/sistemas/menuSistemas.php
+++ ospim/sistemas/menuSistemas.php
@@ -21,47 +21,47 @@ if ($_SESSION['usuario'] != 'sistemas') {
   <table width="900" border="3" style="text-align: center;vertical-align: middle;">
     <tr>
       <td width="200">
-      	<p>Fiscalizaci&oacute;n</p>
+      	<p>FISCALIZACION</p>
         <p><a class="enlace" href="fiscalizacion/menuFiscalizacion.php"><img src="img/fiscalizacion.png" width="90" height="90" border="0" alt="enviar"/></a></p>
       </td>
       <td width="200">
-      	<p>Aplicativo DDJJ </p>
+      	<p>APLICATIVO DDJJ </p>
       	<p><a class="enlace" href="aplicativoddjj/menuAplicativoddjj.php"><img src="img/aplicativoddjj.png" width="97" height="85" border="0" alt="enviar"/></a></p>
       </td>
       <td width="200">
-      	<p>Padrones</p>
+      	<p>PADRONES</p>
         <p><a class="enlace" href="padrones/menuPadrones.php"><img src="img/padrones.png" width="90" height="90" border="0" alt="enviar"/></a></p>
       </td>
       <td width="200">
-      	<p>Stock</p>
+      	<p>STOCK</p>
       	<p><a class="enlace" href="stock/menuStock.php"><img src="img/stock.png" width="90" height="90" border="0" alt="enviar"/></a></p>
       </td>
     </tr>
     <tr>
       <td>
-      	<p>Tratamiento A.F.I.P.</p>
+      	<p>TRATAMIENTO A.F.I.P.</p>
         <p><a class="enlace" href="afip/menuAfip.php"><img src="img/afip.png" width="110" height="90" border="0" alt="enviar"/></a></p>
       </td>
        <td>
-      	<p>Desempleo</p>
+      	<p>DESEMPLEO</p>
       	<p><a class="enlace" href="desempleo/menuDesempleo.php"><img src="img/anses.png" width="90" height="90" border="0" alt="enviar"/></a></p>
       </td>
       <td>
-      	<p>Medicamentos (Alfa Beta)</p>
+      	<p>MEDICAMENTOS</p>
       	<p><a class="enlace" href="medicamentos/menuMedicamentos.php"><img src="img/medicamentos.png" width="90" height="90" border="0" alt="enviar"/></a></p>
       </td>
       <td>
-      	<p>Intranets</p>
+      	<p>INTRANETS</p>
         <p><a class="enlace" href="intranets/menuIntranet.php"><img src="img/intranets.png" width="90" height="90" border="0" alt="enviar"/></a></p>
       </td>
     </tr>
     <tr>
       <td>
-      	<p>Usuarios</p>
+      	<p>USUARIOS</p>
         <p><a href="usuarios/menuUsuarios.php"><img src="img/users.png" width="90" height="90" border="0" alt="enviar"/></a></p>
       </td>
       <td>
-      	<p>Estado Contable</p>
+      	<p>ESTADOS CONTABLES</p>
         <p><a href="estadocontable/estadoContable.php"><img src="img/estadocontable.png" width="90" height="90" border="0" alt="enviar"/></a></p>
       </td>
       <td>
