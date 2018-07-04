dd4f6d9df625697bbc085bf5d623a50b3ad15864
tanomartin <tanomartin05@gmail.com>
2018-07-04 11:54:13 -0300

    corremos a 1 mes el desempleo

--- ospim/afiliados/procesos/filtroTitulares/filtroTitulares.php
+++ ospim/afiliados/procesos/filtroTitulares/filtroTitulares.php
@@ -23,7 +23,7 @@ $fechaInicio = date ( 'Y-m-d', $fechaInicio );
 //echo $fechaInicio . "<br>";
 
 $fechaDesempleo = date('Y')."-".date('m')."-01";
-$fechaDesempleo = strtotime ( '-2 month', strtotime ( $fechaDesempleo ) );
+$fechaDesempleo = strtotime ( '-1 month', strtotime ( $fechaDesempleo ) );
 $fechaDesempleo = date ( 'Y-m-d', $fechaDesempleo );
 //echo $fechaDesempleo . "<br>";
 
