7a65846490a8cf905e994c2784c79a0d063eb273
tanomartin <tanomartin05@gmail.com>
2018-07-05 11:19:42 -0300

    arreglo menu de prestadores sacamos liquidacion que esta afuera del modulo

--- ospim/auditoria/prestadores/menuPrestadores.php
+++ ospim/auditoria/prestadores/menuPrestadores.php
@@ -11,15 +11,11 @@ include($libPath."controlSessionOspim.php"); ?>
 <div align="center">
   <p><input type="button" name="volver" value="Volver" onclick="location.href = '../menuAuditoria.php'"/></p>
   <h3>Menú Prestadores </h3>
-  <table width="600" border="1" style="text-align: center">
+  <table width="400" border="1" style="text-align: center">
   	<tr>
 	  <td width="200">
 	  	<p>A.B.M.C.</p>
         <p><a href="abm/moduloAbmPrestadores.php"><img src="img/prestador.png" width="90" height="90" border="0"/></a></p>
-      </td>
-      <td width="200">
-      	<p>AUDITORIA FACTURA </p>
-        <p><a href="../../moduloNoDisponible.php"><img src="img/auditoria.png" width="90" height="90" border="0"/></a></p>
       </td>
       <td width="200">
       	<p>CUENTA CORRIENTE </p>
