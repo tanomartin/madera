4d00a754b741eb346ef1d1b42003b18ec9afd84f
tanomartin <tanomartin05@gmail.com>
2019-01-22 12:35:46 -0300

    filtro cuentas de imp contable donde sale el dinero

--- ospim/tesoreria/ordenes/ordenpagonm/abm/buscaCuenta.php
+++ ospim/tesoreria/ordenes/ordenpagonm/abm/buscaCuenta.php
@@ -3,7 +3,7 @@ include($libPath."controlSessionOspim.php");
 if(isset($_GET['getCuenta'])) {
 	$busqueda = $_GET['getCuenta'];
 	$cuentas = array();  
-	$sqlCuenta = "SELECT * FROM cuentasospim WHERE nrocta like '%$busqueda%' or titulo like '%$busqueda%' or descripcion like '%$busqueda%'";
+	$sqlCuenta = "SELECT * FROM cuentasospim WHERE titulo = 'BANCO NACION ARGENTINA' and (nrocta like '%$busqueda%' or titulo like '%$busqueda%' or descripcion like '%$busqueda%')";
 	$resCuenta = mysql_query($sqlCuenta,$db);
 	$canCuenta = mysql_num_rows($resCuenta);
 	if ($canCuenta > 0) {
