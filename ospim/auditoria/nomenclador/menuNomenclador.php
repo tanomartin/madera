02f04dbc16ed7c4b9b8f411ff78e626c8b525632
tanomartin <tanomartin05@gmail.com>
2019-11-01 10:55:54 -0300

    Alta modulo resoluciones

--- ospim/auditoria/nomenclador/menuNomenclador.php
+++ ospim/auditoria/nomenclador/menuNomenclador.php
@@ -1,5 +1,5 @@
 <?php $libPath = $_SERVER['DOCUMENT_ROOT']."/madera/lib/";
-include($libPath."controlSessionOspim.php");  ?>
+include($libPath."controlSessionOspim.php"); ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -12,7 +12,7 @@ include($libPath."controlSessionOspim.php");  ?>
 <div align="center">
   <p><input type="reset" name="volver" value="Volver" onclick="location.href = '../menuAuditoria.php'"/></p>
   <h3>Men&uacute; Nomencladores </h3>
-  <table width="600" border="3" style="text-align: center">
+  <table width="400" border="3" style="text-align: center">
     <tr>
 	  <td width="200">
 	  	<p>NOMENCLADOS</p>
@@ -22,9 +22,15 @@ include($libPath."controlSessionOspim.php");  ?>
       	<p>NO NOMENCLADO</p>
         <p><a href="nonomenclado/menuNoNomenclado.php"><img src="img/nomenclador.png" width="90" height="90" border="0" alt="enviar"/></a></p>
       </td>
-      <td width="200">
+     </tr>
+     <tr>
+      <td>
         <p>BUSCADOR</p>
         <p><a href="buscador/buscadorPractica.php"><img src="img/buscar.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+      </td>
+        <td>
+        <p>RESOLUCIONES</p>
+        <p><a href="resoluciones/moduloResoluciones.php"><img src="img/resoluciones.png" width="90" height="90" border="0" alt="enviar"/></a></p>
       </td>
    </tr>
   </table>
