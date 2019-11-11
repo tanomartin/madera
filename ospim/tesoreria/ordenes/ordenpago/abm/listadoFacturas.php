d2f3ad6436b69c4497ef89385c6ede29d5f03f67
tanomartin <tanomartin05@gmail.com>
2019-11-11 10:51:26 -0300

    Agregado de control de emision de pago para generar las ordenes de pago

--- ospim/tesoreria/ordenes/ordenpago/abm/listadoFacturas.php
+++ ospim/tesoreria/ordenes/ordenpago/abm/listadoFacturas.php
@@ -45,6 +45,7 @@ $sqlFacPendientesInte = "SELECT
 							f.idPrestador = ".$rowPrestador['codigoprestador']." and
 							f.fechacierreliquidacion is not null and
 							f.restoapagar > 0 and 
+							f.autorizacionpago = 1
 							f.id = p.idFactura and p.id = i.idFacturaPrestacion
 						 ORDER BY f.fechacomprobante ASC";
 $resFacPendientesInte = mysql_query($sqlFacPendientesInte,$db);
@@ -65,7 +66,8 @@ $sqlFacPendientes = "SELECT
 					 WHERE
 						f.idPrestador = ".$rowPrestador['codigoprestador']." and
 						f.fechacierreliquidacion is not null and
-						f.restoapagar > 0
+						f.restoapagar > 0 and 
+						f.autorizacionpago = 1
 					 ORDER BY f.fechacomprobante ASC";
 $resFacPendientes = mysql_query($sqlFacPendientes,$db);
 $canFacPendientes = mysql_num_rows($resFacPendientes);
