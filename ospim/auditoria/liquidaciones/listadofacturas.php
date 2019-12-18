19a67efc5b39fba9b8e3f54d0edcb21b336aa358
tanomartin <tanomartin05@gmail.com>
2019-12-18 09:56:51 -0300

    Subida listado facturas liquidadas

--- ospim/auditoria/liquidaciones/listadofacturas.php
+++ ospim/auditoria/liquidaciones/listadofacturas.php
@@ -26,8 +26,7 @@ if ($numFacturasInte > 0) {
 	while ($rowFacturaInte = mysql_fetch_assoc($resFacturasInte)) {
 		$arrayInte[$rowFacturaInte['id']] = $rowFacturaInte['id'];
 	}
-}
-?>
+} ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
