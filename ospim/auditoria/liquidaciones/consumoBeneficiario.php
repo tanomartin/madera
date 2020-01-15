8f75a28c72ce7053b2a629e1a5c00a4586017691
Juan Carlos Bolognese <jcbolognese@gmail.com>
2020-01-15 13:42:57 -0300

    Se invierte logica para calculo de coseguro por tercera vez

--- ospim/auditoria/liquidaciones/consumoBeneficiario.php
+++ ospim/auditoria/liquidaciones/consumoBeneficiario.php
@@ -666,17 +666,17 @@ $(document).ready(function(){
 		if($("#incluyecoseguro").prop('checked') ) {
 			infoconformado = infoconformado.replace(' + Coseguro','');
 			$('#infoconformado').attr('title', infoconformado);
-			var nuevovalor = parseFloat($("#referenciacoseguro").val()) - parseFloat($("#valorCoseguro").val());
+			var nuevovalor = parseFloat($("#referenciacoseguro").val()) + parseFloat($("#valorCoseguro").val());
+			$("#referenciacoseguro").val(nuevovalor);			
 			var valorconformado = parseFloat($("#referenciaconformado").val()) - parseFloat($("#referenciacoseguro").val());
-			$("#referenciacoseguro").val(nuevovalor);
 			$("#referenciaconformado").val(valorconformado);
 			$("#valorCoseguro").attr('disabled', true);
 		} else {
 			infoconformado = infoconformado+' + Coseguro';
 			$('#infoconformado').attr('title', infoconformado);
-			var nuevovalor = parseFloat($("#referenciacoseguro").val()) + parseFloat($("#valorCoseguro").val());
-			$("#referenciacoseguro").val(nuevovalor);
+			var nuevovalor = parseFloat($("#referenciacoseguro").val()) - parseFloat($("#valorCoseguro").val());
 			var valorconformado = parseFloat($("#referenciaconformado").val()) + parseFloat($("#referenciacoseguro").val());
+			$("#referenciacoseguro").val(nuevovalor);
 			$("#referenciaconformado").val(valorconformado);
 			$("#valorCoseguro").attr('disabled', false);
 		}
@@ -1283,7 +1283,7 @@ function anulaConsumoCarencia(idconsumocarencia, idfactura, idfacturabeneficiari
 			<tr id="coseguro" style="display:none">
 				<td align="right" colspan="2"><strong>Coseguro a Cargo Beneficiario?</strong></td>
 				<td align="left" colspan="4"><input name="incluyecoseguro" type="checkbox" id="incluyecoseguro" value="1"/>
-					| Valor Coseguro <input name="valorCoseguro" type="text" id="valorCoseguro" size="5" readonly="readonly" style="background-color:#CCCCCC" value=""/> |<input name="referenciacoseguro" type="hidden" id="referenciacoseguro" size="5" value="0.00"/>
+					| Valor Coseguro <input name="valorCoseguro" type="text" id="valorCoseguro" size="5" readonly="readonly" style="background-color:#CCCCCC" value=""/> |<input name="referenciacoseguro" type="text" id="referenciacoseguro" size="5" value="0.00"/>
 				</td>
 			</tr>
 			<tr id="calidadestablecimiento" style="display:none">
